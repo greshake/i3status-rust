9449be8389403dd6ce458d523ede285fd12d10a5
XYunknown <privat@kai-greshake.de>
2017-04-17 16:31:25 +0200

    unregisterd the template block

--- src/blocks/mod.rs
+++ src/blocks/mod.rs
@@ -1,14 +1,9 @@
 pub mod cpu;
 pub mod disk_info;
-pub mod template;
 pub mod time;
 pub mod toggle;
 
-use self::cpu::*;
 use self::time::*;
-use self::disk_info::*;
-use self::toggle::*;
-use self::template::*;
 
 use super::block::Block;
 
@@ -20,7 +15,6 @@ macro_rules! boxed ( { $b:expr } => { Box::new($b) as Box<Block> }; );
 pub fn create_block(name: &str, config: Value) -> Box<Block> {
     match name {
         "time" => boxed!(Time::new(config)),
-        "template" => boxed!(Template::new(config)),
         _ => {
             panic!("Ńot a registered block: {}", name);
         }
