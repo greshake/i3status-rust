7a50f591a94b6ff780b8bce83adc46c7f5017cb5
Heyens <jens.heyens@cispa.saarland>
2017-05-19 21:09:19 +0200

    Update memory module ("v1")

--- src/blocks/memory.rs
+++ src/blocks/memory.rs
@@ -1,10 +1,46 @@
-//! This is a simple module for i3status-rust implementing a basic memory usage Widget.
+//! ##Memory
+//! Creates a block displaying memory and swap usage.
 //!
-//! By default, the format of this
+//! By default, the format of this module is "<Icon>: {MFm}MB/{MTm}MB({Mp}%)" (Swap values
+//! accordingly). That behaviour can be changed within config.json.
+//!
+//! This module keeps track of both Swap and Memory. By default, a click switches between them.
+//!
+//! **Example**
+//! ```javascript
+//! {"block": "memory",
+//!     "format_mem": "{MFm}MB/{MTm}MB({Mp}%)", "format_swap": "{SFm}MB/{STm}MB({Sp}%)",
+//!     "type": "memory", "icons": "true", "clickable": "true", "interval": "5"
+//! },
+//! ```
+//!
+//! **Options**
+//! Key | Values | Required | Default
+//! ----|--------|----------|--------
+//! format_mem | Format string for Memory view. All format values are described below. | No | {MFm}MB/{MTm}MB({Mp}%)
+//! format_swap | Format string for Swap view. | No | {SFm}MB/{STm}MB({Sp}%)
+//! type | Default view displayed on startup. Options are <br/> memory, swap | No | memory
+//! icons | Whether the format string should be prepended with Icons. Options are <br/> true, false | No | true
+//! clickable | Whether the view should switch between memory and swap on click. Options are <br/> true, false | No | true
+//! interval | The delay in seconds between an update. If `clickable`, an update is triggered on click. Integer values only. | No | 5
+//!
+//! ###Format string specification
+//! Key | Values
+//! ----|-------
+//! {MTg} | Memory total (GiB)
+//! {MTm} | Memory total (MiB)
+//! {MFg} | Memory free (GiB)
+//! {MFm} | Memory free (MiB)
+//! {Mp} | Memory used (%)
+//! {STg} | Swap total (GiB)
+//! {STm} | Swap total (MiB)
+//! {SFg} | Swap free (GiB)
+//! {SFm} | Swap free (MiB)
+//! {Sp} | Swap used (%)
 //!
 //!
 
-use std::time::Duration;
+use std::time::{Instant, Duration};
 use std::collections::HashMap;
 use util::*;
 use std::sync::mpsc::Sender;
@@ -22,7 +58,7 @@ use widget::I3BarWidget;
 use scheduler::Task;
 
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 enum Memtype {
     SWAP,
     MEMORY
@@ -32,29 +68,16 @@ enum Memtype {
 pub struct Memory {
     name: String,
     memtype: Memtype,
-    output: TextWidget,
+    output: HashMap<Memtype, TextWidget>,
     clickable: bool,
-    format: FormatTemplate,
+    format: HashMap<Memtype, FormatTemplate>,
     update_interval: Duration,
     tx_update_request: Sender<Task>,
-    values: HashMap<String,String>
+    values: HashMap<String, String>
 }
 
 
-
 impl Memory {
-    /// Format replacement:
-    ///     {MTg} - Memory total (GiB)
-    ///     {MTm} - Memory total (MiB)
-    ///     {MFg} - Memory free (GiB)
-    ///     {MFm} - Memory free (MiB)
-    ///     {Mp} - Memory used (%)
-    ///     {STg} - Swap total (GiB)
-    ///     {STm} - Swap total (MiB)
-    ///     {SFg} - Swap free (GiB)
-    ///     {SFm} - Swap free (MiB)
-    ///     {Sp} - Swap used (%)
-    ///
     /// mem_state: (memory_total, memory_free, swap_total, swap_free)
     ///
     fn format_insert_values(&mut self, mem_state: (usize, usize, usize, usize)) -> String {
@@ -79,7 +102,7 @@ impl Memory {
         self.values.insert("{SFg}".to_string(), format!("{}", sfg));
         self.values.insert("{SFm}".to_string(), format!("{}", sfm));
         self.values.insert("{Sp}".to_string(), format!("{:.2}", sp));
-        self.format.render(&self.values)
+        self.format.get(&self.memtype).unwrap().render(&self.values)
     }
 
 
@@ -91,28 +114,39 @@ impl Memory {
         };
     }
     pub fn new(config: Value, tx: Sender<Task>, theme: Value) -> Memory {
-        let _memtype: String = get_str_default!(config, "type", "memory");
+        let memtype: String = get_str_default!(config, "type", "memory");
+        let icons: bool = get_bool_default!(config, "icons", true);
+        let textwidget = TextWidget::new(theme.clone()).with_text("");
         Memory {
-
             name: Uuid::new_v4().simple().to_string(),
-            memtype: match _memtype.as_ref() {
+            memtype: match memtype.as_ref() {
                 "memory" => Memtype::MEMORY,
                 "swap" => Memtype::SWAP,
-                _ => panic!(format!("Invalid Memory type: {}", _memtype))
+                _ => panic!(format!("Invalid Memory type: {}", memtype))
             },
-            output: TextWidget::new(theme.clone()).with_text("").with_icon("memory_mem"),
-            clickable: match get_str_default!(config, "clickable", "true").as_ref() {
-                "true" => true,
-                "false" => false,
-                _ => panic!("Option clickable accepts only boolean values.")
-            },
-            format: match config["format"] {
+            output:
+            if icons {
+                map!(Memtype::SWAP => textwidget.clone().with_icon("memory_swap"),
+                         Memtype::MEMORY => textwidget.with_icon("memory_mem"))
+            } else {
+                map!(Memtype::SWAP => textwidget.clone(),
+                         Memtype::MEMORY => textwidget)
+            }
+            ,
+            clickable: get_bool_default!(config, "clickable", true),
+            format: map!(
+            Memtype::MEMORY => match config["format_mem"] {
                 Value::String(ref e) => {
                     FormatTemplate::from_string(e.clone()).unwrap()
                 },
-                _ =>FormatTemplate::from_string("abcde{Mp}:{MTg}:{Sp}".to_string()).unwrap()
-
-            },
+                _ =>FormatTemplate::from_string("{MFm}MB/{MTm}MB({Mp}%)".to_string()).unwrap()
+            }, Memtype::SWAP => match config["format_swap"] {
+                Value::String(ref e) => {
+                    FormatTemplate::from_string(e.clone()).unwrap()
+                },
+                _ =>FormatTemplate::from_string("{SFm}MB/{STm}MB({Sp}%)".to_string()).unwrap()
+            }
+            ),
             update_interval: Duration::new(get_u64_default!(config, "interval", 5), 0),
             tx_update_request: tx,
             values: map!(
@@ -144,9 +178,9 @@ impl Block for Memory
                                                     Are we on a linux system?");
         let f = BufReader::new(f);
 
-        let (mut mt,mut mf, mut st, mut sf) = (false, false, false, false);
+        let (mut mt, mut mf, mut st, mut sf) = (false, false, false, false);
 
-        let mut mem_state: (usize, usize, usize, usize) = (0,0,0,0);
+        let mut mem_state: (usize, usize, usize, usize) = (0, 0, 0, 0);
 
         let lines = f.lines();
         for line in lines {
@@ -157,9 +191,9 @@ impl Block for Memory
 
             let line = match line {
                 Ok(s) => s,
-                _ => {continue}
+                _ => { continue }
             };
-            let line : Vec<&str>= line.split_whitespace().collect::<Vec<&str>>();
+            let line: Vec<&str> = line.split_whitespace().collect::<Vec<&str>>();
 
 
             match line[0] {
@@ -192,23 +226,8 @@ impl Block for Memory
 
         // Now, create the string to be shown
 
-            let output_text = self.format_insert_values(mem_state);
-            self.output.set_text(output_text);
-//        } else {
-//            match self.memtype {
-//                Memtype::SWAP => {
-//                    self.output.set_icon("memory_swap");
-//                    let sp: f32 = 100f32 - 100f32 * (mem_state.3 as f32) / (mem_state.2 as f32);
-//                    self.output.set_text(format!(": {:.2}%/{:.2}GiB", sp, mem_state.2 as f32 / (1024f32 * 1024f32)));
-//                }
-//                Memtype::MEMORY => {
-//                    self.output.set_icon("memory_mem");
-//                    let mp: f32 = 100f32 - 100f32 * (mem_state.1 as f32) / (mem_state.0 as f32);
-//                    self.output.set_text(format!(": {:.2}%/{:.2}GiB", mp, mem_state.0 as f32 / (1024f32 * 1024f32)));
-//                }
-//            }
-//        }
-
+        let output_text = self.format_insert_values(mem_state);
+        self.output.get_mut(&self.memtype).unwrap().set_text(output_text);
 
         Some(self.update_interval.clone())
     }
@@ -218,10 +237,11 @@ impl Block for Memory
         if self.clickable && event.button == 1 {
             self.switch();
             self.update();
+            self.tx_update_request.send(Task { id: self.name.clone(), update_time: Instant::now() }).ok();
         }
     }
 
     fn view(&self) -> Vec<&I3BarWidget> {
-        vec![&self.output]
+        vec![self.output.get(&self.memtype).unwrap()]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![warn(missing_docs)]
+
 #[macro_use]
 extern crate serde_derive;
 
