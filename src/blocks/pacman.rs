665e09974ec03ea05fd1fb85ff424b0694375814
Pearce Keesling <keeslinp@gmail.com>
2017-05-20 15:28:24 -0600

    cleaned up the pacman block a bit

--- src/blocks/pacman.rs
+++ src/blocks/pacman.rs
@@ -1,12 +1,9 @@
 use std::time::Duration;
-use std::sync::mpsc::Sender;
 use std::process::Command;
 
 use block::Block;
 use widgets::text::TextWidget;
 use widget::I3BarWidget;
-use input::I3barEvent;
-use scheduler::Task;
 
 use serde_json::Value;
 use uuid::Uuid;
@@ -17,15 +14,11 @@ pub struct Pacman {
     id: String,
     update_interval: Duration,
 
-    //useful, but optional
-    #[allow(dead_code)]
     theme: Value,
-    #[allow(dead_code)]
-    tx_update_request: Sender<Task>,
 }
 
 impl Pacman {
-    pub fn new(config: Value, tx: Sender<Task>, theme: Value) -> Pacman {
+    pub fn new(config: Value, theme: Value) -> Pacman {
         {
             Pacman {
                 id: Uuid::new_v4().simple().to_string(),
@@ -42,16 +35,22 @@ impl Pacman {
 impl Block for Pacman
 {
     fn update(&mut self) -> Option<Duration> {
-        let output = String::from_utf8(Command::new("sh").arg("-c").arg("pacman -Sup 2>/dev/null").output().expect("You need to have pacman set up").stdout).expect("crazy stuff");
+        let output = String::from_utf8(
+            Command::new("sh")
+                .arg("-c")
+                .arg("pacman -Sup 2>/dev/null")
+                .output()
+                .expect("You need to have pacman set up")
+                .stdout
+            ).expect("Something is wrong with the pacman output.");
         let count = output.lines().count() - 1;
-        self.text.set_text(format!("{} updates", count));
+        self.text.set_text(format!("{}", count));
         self.text.set_icon("update");
         Some(self.update_interval.clone())
     }
     fn view(&self) -> Vec<&I3BarWidget> {
         vec![&self.text]
     }
-    fn click(&mut self, _: &I3barEvent) {}
     fn id(&self) -> &str {
         &self.id
     }
