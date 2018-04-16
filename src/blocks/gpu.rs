eb2cf4b8a0a1e1996b43762d58ab3a8b3c7d41c2
Igor <igoshaaaaaa@mail.ru>
2018-04-16 04:13:43 +0000

    ready. for closed nvidia driver

--- src/blocks/gpu.rs
+++ src/blocks/gpu.rs
@@ -8,7 +8,6 @@ use de::deserialize_duration;
 use errors::*;
 use widgets::text::TextWidget;
 use widget::I3BarWidget;
-//use input::I3BarEvent;
 use scheduler::Task;
 
 use uuid::Uuid;
@@ -19,7 +18,14 @@ pub struct Gpu {
     update_interval: Duration,
 
     gpu_id: u64,
+    vendor: String,
+    driver: String,
+    label: String,
     utilization: Option<TextWidget>,
+    memory: Option<TextWidget>,
+    temperature: Option<TextWidget>,
+    fan_speed: Option<TextWidget>,
+    clocks: Option<TextWidget>,
 }
 
 #[derive(Deserialize, Debug, Default, Clone)]
@@ -29,6 +35,18 @@ pub struct GpuConfig {
     #[serde(default = "GpuConfig::default_interval", deserialize_with = "deserialize_duration")]
     pub interval: Duration,
 
+    /// Label
+    #[serde(default = "GpuConfig::default_label")]
+    pub label: String,
+
+    /// Vendor
+    #[serde(default = "GpuConfig::default_vendor")]
+    pub vendor: String,
+
+    /// Driver version
+    #[serde(default = "GpuConfig::default_driver")]
+    pub driver: String,
+
     /// GPU id in system
     #[serde(default = "GpuConfig::default_gpu_id")]
     pub gpu_id: u64,
@@ -36,6 +54,22 @@ pub struct GpuConfig {
     /// GPU utilization. In percents.
     #[serde(default = "GpuConfig::default_utilization")]
     pub utilization: bool,
+
+    /// VRAM utilization.
+    #[serde(default = "GpuConfig::default_memory")]
+    pub memory: bool,
+
+    /// Core GPU temperature. In degrees C.
+    #[serde(default = "GpuConfig::default_temperature")]
+    pub temperature: bool,
+
+    /// Fan speed.
+    #[serde(default = "GpuConfig::default_fan_speed")]
+    pub fan_speed: bool,
+
+    /// GPU clocks. In percents.
+    #[serde(default = "GpuConfig::default_clocks")]
+    pub clocks: bool,
 }
 
 impl GpuConfig {
@@ -43,11 +77,39 @@ impl GpuConfig {
         Duration::from_secs(5)
     }
 
+    fn default_label() -> String {
+        "".to_string()
+    }
+
+    fn default_vendor() -> String {
+        "nvidia".to_string()
+    }
+
+    fn default_driver() -> String {
+        "closed".to_string()
+    }
+
     fn default_gpu_id() -> u64 {
         0
     }
 
     fn default_utilization() -> bool {
+        true
+    }
+
+    fn default_memory() -> bool {
+        true
+    }
+
+    fn default_temperature() -> bool {
+        true
+    }
+
+    fn default_fan_speed() -> bool {
+        true
+    }
+
+    fn default_clocks() -> bool {
         true
     }
 }
@@ -61,9 +123,28 @@ impl ConfigBlock for Gpu {
             update_interval: block_config.interval,
             gpu_widget: TextWidget::new(config.clone()).with_icon("gpu"),
             // TODO
-            // Add params
+            // Add open source drivers
             gpu_id: block_config.gpu_id,
+            vendor: block_config.vendor,
+            driver: block_config.driver,
+            label: block_config.label,
             utilization: match block_config.utilization {
+                true => Some(TextWidget::new(config.clone())),
+                false => None,
+            },
+            memory: match block_config.memory {
+                true => Some(TextWidget::new(config.clone())),
+                false => None,
+            },
+            temperature: match block_config.temperature {
+                true => Some(TextWidget::new(config.clone())),
+                false => None,
+            },
+            fan_speed: match block_config.fan_speed {
+                true => Some(TextWidget::new(config.clone())),
+                false => None,
+            },
+            clocks: match block_config.clocks {
                 true => Some(TextWidget::new(config.clone())),
                 false => None,
             },
@@ -73,7 +154,31 @@ impl ConfigBlock for Gpu {
 
 impl Block for Gpu {
     fn update(&mut self) -> Result<Option<Duration>> {
-        let params = "utilization.gpu";
+        // TODO
+        // Add open source drivers
+        if self.vendor != "nvidia" || self.driver != "closed" {
+            self.gpu_widget.set_text(format!("Invalid config. Do not use AMD and open drivers"));
+
+            return Ok(Some(self.update_interval));
+        }
+
+        let mut params = String::new();
+        if self.utilization.is_some() {
+            params += "utilization.gpu,";
+        }
+        if self.memory.is_some() {
+            params += "memory.used,memory.total,";
+        }
+        if self.temperature.is_some() {
+            params += "temperature.gpu,";
+        }
+        if self.fan_speed.is_some() {
+            params += "fan.speed,";
+        }
+        if self.clocks.is_some() {
+            params += "clocks.current.graphics,";
+        }
+
         let mut output = Command::new("nvidia-smi")
             .args(
                 &[
@@ -86,14 +191,31 @@ impl Block for Gpu {
             .block_error("gpu", "Failed to execute nvidia-smi.")?
             .stdout;
         output.pop(); // Remove trailing newline.
-        let result = String::from_utf8(output).unwrap();
+        let result_str = String::from_utf8(output).unwrap();
+        let result: Vec<&str> = result_str.split(", ").collect();
 
+        let mut count: usize = 0;
         if let Some(ref mut utilization_widget) = self.utilization {
-            utilization_widget.set_text(format!("{:02}%", result));
+            utilization_widget.set_text(format!("{:02}%", result[count]));
+            count += 1;
+        }
+        if let Some(ref mut memory_widget) = self.memory {
+            memory_widget.set_text(format!("{}MB/{}MB", result[count], result[count + 1]));
+            count += 2;
+        }
+        if let Some(ref mut temperature_widget) = self.temperature {
+            temperature_widget.set_text(format!("{:02}°C", result[count]));
+            count += 1;
+        }
+        if let Some(ref mut fan_speed_widget) = self.fan_speed {
+            fan_speed_widget.set_text(format!("{:02}%", result[count]));
+            count += 1;
+        }
+        if let Some(ref mut clocks_widget) = self.clocks {
+            clocks_widget.set_text(format!("{}MHz", result[count]));
         }
 
-        self.gpu_widget.set_text(format!("gpu {}",
-                                         self.gpu_id,));
+        self.gpu_widget.set_text(format!("{}", self.label));
 
         Ok(Some(self.update_interval))
     }
@@ -103,6 +225,18 @@ impl Block for Gpu {
         widgets.push(&self.gpu_widget);
         if let Some(ref utilization_widget) = self.utilization {
             widgets.push(utilization_widget);
+        }
+        if let Some(ref memory_widget) = self.memory {
+            widgets.push(memory_widget);
+        }
+        if let Some(ref temperature_widget) = self.temperature {
+            widgets.push(temperature_widget);
+        }
+        if let Some(ref fan_speed_widget) = self.fan_speed {
+            widgets.push(fan_speed_widget);
+        }
+        if let Some(ref clocks_widget) = self.clocks {
+            widgets.push(clocks_widget);
         }
         widgets
     }
