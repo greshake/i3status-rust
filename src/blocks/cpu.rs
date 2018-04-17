a631cdab42601a827f5b4bd7e5bb046542413380
Matthias Krüger <matthias.krueger@famsik.de>
2018-04-17 12:23:13 +0200

    cpu block: add option to display average cpu frequency of all cores, defaults to false.

--- src/blocks/cpu.rs
+++ src/blocks/cpu.rs
@@ -1,17 +1,17 @@
-use std::time::Duration;
 use chan::Sender;
 use scheduler::Task;
+use std::time::Duration;
 
 use block::{Block, ConfigBlock};
 use config::Config;
 use de::deserialize_duration;
 use errors::*;
-use widgets::text::TextWidget;
 use widget::{I3BarWidget, State};
+use widgets::text::TextWidget;
 
-use std::io::BufReader;
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 use uuid::Uuid;
 
@@ -24,6 +24,7 @@ pub struct Cpu {
     minimum_info: u64,
     minimum_warning: u64,
     minimum_critical: u64,
+    frequency: bool,
 }
 
 #[derive(Deserialize, Debug, Default, Clone)]
@@ -44,6 +45,10 @@ pub struct CpuConfig {
     /// Minimum usage, where state is set to critical
     #[serde(default = "CpuConfig::default_critical")]
     pub critical: u64,
+
+    /// Display frequency
+    #[serde(default = "CpuConfig::default_frequency")]
+    pub frequency: bool,
 }
 
 impl CpuConfig {
@@ -62,6 +67,10 @@ impl CpuConfig {
     fn default_critical() -> u64 {
         90
     }
+
+    fn default_frequency() -> bool {
+        false
+    }
 }
 
 impl ConfigBlock for Cpu {
@@ -77,27 +86,39 @@ impl ConfigBlock for Cpu {
             minimum_info: block_config.info,
             minimum_warning: block_config.warning,
             minimum_critical: block_config.critical,
+            frequency: block_config.frequency,
         })
     }
 }
 
 impl Block for Cpu {
     fn update(&mut self) -> Result<Option<Duration>> {
-        let f = File::open("/proc/stat")
-            .block_error("cpu", "Your system doesn't support /proc/stat")?;
+        let f = File::open("/proc/stat").block_error("cpu", "Your system doesn't support /proc/stat")?;
         let f = BufReader::new(f);
 
+        let mut freq: f32 = 0.0;
+        if self.frequency {
+            let freq_file = File::open("/proc/cpuinfo").block_error("cpu", "failed to read /proc/cpuinfo")?;
+            let freq_file_content = BufReader::new(freq_file);
+            let mut cores = 0;
+            // read frequency of each cpu and calculate the average which we will display
+            for line in freq_file_content.lines().scan((), |_, x| x.ok()) {
+                if line.starts_with("cpu MHz") {
+                    cores += 1;
+                    let words = line.split(' ');
+                    let last = words.last().expect("failed to get last word of line while getting cpu frequency");
+                    let numb = last.parse::<f32>().expect("failed to parse String to f32 while getting cpu frequency");
+                    freq += numb;
+                }
+            }
+            // get the average
+            freq = (freq / (cores as f32) / 1000.0) as f32;
+        }
         let mut utilization = 0;
 
         for line in f.lines().scan((), |_, x| x.ok()) {
             if line.starts_with("cpu ") {
-                let data: Vec<u64> = (&line)
-                    .split(' ')
-                    .collect::<Vec<&str>>()
-                    .iter()
-                    .skip(2)
-                    .filter_map(|x| x.parse::<u64>().ok())
-                    .collect::<Vec<_>>();
+                let data: Vec<u64> = (&line).split(' ').collect::<Vec<&str>>().iter().skip(2).filter_map(|x| x.parse::<u64>().ok()).collect::<Vec<_>>();
 
                 // idle = idle + iowait
                 let idle = data[3] + data[4];
@@ -133,9 +154,12 @@ impl Block for Cpu {
             x if x > self.minimum_info => State::Info,
             _ => State::Idle,
         });
-
-        self.utilization.set_text(format!("{:02}%", utilization));
-
+        if self.frequency {
+            let frequency = format!("{:.*}", 1, freq);
+            self.utilization.set_text(format!("{:02}% {}GHz", utilization, frequency));
+        } else {
+            self.utilization.set_text(format!("{:02}%", utilization));
+        }
         Ok(Some(self.update_interval))
     }
 
