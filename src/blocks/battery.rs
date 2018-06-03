553b27fc4a3bc54a252c76bed4af306b70bb2daa
Aaron Jacobs <atheriel@gmail.com>
2018-06-02 22:43:03 -0400

    Abstracts some battery queries into a new power supply device API.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- src/blocks/battery.rs
+++ src/blocks/battery.rs
@@ -1,21 +1,161 @@
+use std::path::{Path, PathBuf};
 use std::time::Duration;
+
 use chan::Sender;
-use scheduler::Task;
+use uuid::Uuid;
+
 use block::{Block, ConfigBlock};
 use config::Config;
 use de::deserialize_duration;
 use errors::*;
-use widgets::text::TextWidget;
+use scheduler::Task;
+use util::read_file;
 use widget::{I3BarWidget, State};
-use uuid::Uuid;
-use blocks::lib::*;
+use widgets::text::TextWidget;
+
+/// Represents a physical power supply device.
+pub struct PowerSupplyDevice {
+    device_path: PathBuf,
+    charge_full: Option<u64>,
+    energy_full: Option<u64>,
+}
+
+impl PowerSupplyDevice {
+    /// Use the power supply device `device`. Raises an error if a directory for
+    /// that device is not found.
+    pub fn from_device(device: String) -> Result<Self> {
+        let device_path = Path::new("/sys/class/power_supply").join(device.clone());
+        if !device_path.exists() {
+            return Err(BlockError(
+                "battery".to_string(),
+                format!(
+                    "Power supply device '{}' does not exist",
+                    device_path.to_string_lossy()
+                ),
+            ));
+        }
+
+        // Read charge_full exactly once, if it exists.
+        let charge_full = if device_path.join("charge_full").exists() {
+            Some(read_file("battery", &device_path.join("charge_full"))?
+                .parse::<u64>()
+                .block_error("battery", "failed to parse charge_full")?)
+        } else {
+            None
+        };
+
+        // Read energy_full exactly once, if it exists.
+        let energy_full = if device_path.join("energy_full").exists() {
+            Some(read_file("battery", &device_path.join("energy_full"))?
+                .parse::<u64>()
+                .block_error("battery", "failed to parse energy_full")?)
+        } else {
+            None
+        };
+
+        Ok(PowerSupplyDevice {
+            device_path: device_path,
+            charge_full: charge_full,
+            energy_full: energy_full,
+        })
+    }
+
+    /// Query the device status.
+    pub fn status(&self) -> Result<String> {
+        read_file("battery", &self.device_path.join("status"))
+    }
+
+    /// Query the device's current capacity, as a percent.
+    pub fn capacity(&self) -> Result<u64> {
+        let capacity_path = self.device_path.join("capacity");
+        let charge_path = self.device_path.join("charge_now");
+        let energy_path = self.device_path.join("energy_now");
+
+        let capacity = if capacity_path.exists() {
+            read_file("battery", &capacity_path)?
+                .parse::<u64>()
+                .block_error("battery", "failed to parse capacity")?
+        } else if charge_path.exists() && self.charge_full.is_some() {
+            let charge = read_file("battery", &charge_path)?
+                .parse::<u64>()
+                .block_error("battery", "failed to parse charge_now")?;
+            ((charge as f64 / self.charge_full.unwrap() as f64) * 100.0) as u64
+        } else if energy_path.exists() && self.energy_full.is_some() {
+            let charge = read_file("battery", &energy_path)?
+                .parse::<u64>()
+                .block_error("battery", "failed to parse energy_now")?;
+            ((charge as f64 / self.energy_full.unwrap() as f64) * 100.0) as u64
+        } else {
+            return Err(BlockError(
+                "battery".to_string(),
+                "Device does not support reading capacity, charge, or energy".to_string(),
+            ));
+        };
+
+        match capacity {
+            0...100 => Ok(capacity),
+            // We need to cap it at 100, because the kernel may report
+            // charge_now same as charge_full_design when the battery is full,
+            // leading to >100% charge.
+            _ => Ok(100),
+        }
+    }
+
+    /// Query the estimated time remaining, in minutes, before (dis)charging is
+    /// complete.
+    pub fn time_remaining(&self) -> Result<u64> {
+        let energy_full = match self.energy_full {
+            Some(val) => val,
+            None => {
+                return Err(BlockError(
+                    "battery".to_string(),
+                    "Device does not support reading energy".to_string(),
+                ))
+            }
+        };
+        let energy_path = self.device_path.join("energy_now");
+        let energy_now = if energy_path.exists() {
+            read_file("battery", &energy_path)?
+                .parse::<f64>()
+                .block_error("battery", "failed to parse energy_now")?
+        } else {
+            return Err(BlockError(
+                "battery".to_string(),
+                "Device does not support reading energy".to_string(),
+            ));
+        };
+        let power_path = self.device_path.join("power_now");
+        let power_now = if power_path.exists() {
+            read_file("battery", &power_path)?
+                .parse::<f64>()
+                .block_error("battery", "failed to parse power_now")?
+        } else {
+            return Err(BlockError(
+                "battery".to_string(),
+                "Device does not support reading power".to_string(),
+            ));
+        };
+        let status = self.status()?;
+        match status.as_str() {
+            "Full" => Ok(((energy_full as f64 / power_now) * 60.0) as u64),
+            "Discharging" => Ok(((energy_now / power_now) * 60.0) as u64),
+            "Charging" => Ok((((energy_full as f64 - energy_now) / power_now) * 60.0) as u64),
+            _ => {
+                // TODO: What should we return in this case? It seems that under
+                // some conditions sysfs will return 0 for some readings (energy
+                // or power), so perhaps the most natural thing to do is emulate
+                // that.
+                Ok(0)
+            }
+        }
+    }
+}
 
 pub struct Battery {
     output: TextWidget,
     id: String,
-    max_charge: u64,
     update_interval: Duration,
-    device_path: String,
+    device: PowerSupplyDevice,
     show: ShowType,
 }
 
@@ -63,10 +203,9 @@ impl ConfigBlock for Battery {
     fn new(block_config: Self::Config, config: Config, _tx_update_request: Sender<Task>) -> Result<Self> {
         Ok(Battery {
             id: Uuid::new_v4().simple().to_string(),
-            max_charge: 0,
             update_interval: block_config.interval,
             output: TextWidget::new(config),
-            device_path: format!("/sys/class/power_supply/{}/", block_config.device),
+            device: try!(PowerSupplyDevice::from_device(block_config.device)),
             show: block_config.show,
         })
     }
@@ -76,104 +215,13 @@ impl Block for Battery {
     fn update(&mut self) -> Result<Option<Duration>> {
         // TODO: Maybe use dbus to immediately signal when the battery state changes.
 
-        let mut current_percentage: u64;
+        let current_percentage = self.device.capacity()?;
 
-        if file_exists(&format!("{}capacity", self.device_path)) {
-            current_percentage = match read_file("battery", &format!("{}capacity", self.device_path))?
-                .parse::<u64>()
-                .block_error("battery", "failed to parse capacity")?
-            {
-                capacity if capacity < 100 => capacity,
-                _ => 100,
-            }
-        } else if file_exists(&format!("{}charge_full", self.device_path)) && file_exists(&format!("{}charge_now", self.device_path)) {
-            // We only need to read max_charge once, shouldn't change
-            if self.max_charge == 0 {
-                self.max_charge = read_file("battery", &format!("{}charge_full", self.device_path))?
-                    .parse::<u64>()
-                    .block_error("battery", "failed to parse charge_full")?;
-            }
+        let state = self.device.status()?;
 
-            let current_charge = read_file("battery", &format!("{}charge_now", self.device_path))?
-                .parse::<u64>()
-                .block_error("battery", "failed to parse charge_now")?;
-            current_percentage = ((current_charge as f64 / self.max_charge as f64) * 100.0) as u64;
-            current_percentage = match current_percentage {
-                0...100 => current_percentage,
-                // We need to cap it at 100, because the kernel may report
-                // charge_now same as charge_full_design when the battery
-                // is full, leading to >100% charge.
-                _ => 100,
-            };
-        } else if file_exists(&format!("{}energy_full", self.device_path)) && file_exists(&format!("{}energy_now", self.device_path)) {
-            // We only need to read max_charge once, shouldn't change
-            if self.max_charge == 0 {
-                self.max_charge = read_file("battery", &format!("{}energy_full", self.device_path))?
-                    .parse::<u64>()
-                    .block_error("battery", "failed to parse energy_full")?;
-            }
-
-            let current_charge = read_file("battery", &format!("{}energy_now", self.device_path))?
-                .parse::<u64>()
-                .block_error("battery", "failed to parse energy_now")?;
-            current_percentage = ((current_charge as f64 / self.max_charge as f64) * 100.0) as u64;
-            current_percentage = match current_percentage {
-                0...100 => current_percentage,
-                // We need to cap it at 100, because the kernel may report
-                // charge_now same as charge_full_design when the battery
-                // is full, leading to >100% charge.
-                _ => 100,
-            };
-        }
-        
-         else {
-            return Err(BlockError(
-                "battery".to_string(),
-                "Device does not support reading capacity, charge or energy".to_string(),
-            ));
-        }
-
-        let state = read_file("battery", &format!("{}status", self.device_path))?;
-
-        let energy_now = if file_exists(&format!("{}energy_now", self.device_path)) {
-            read_file("battery", &format!("{}energy_now", self.device_path))?
-                .parse::<u64>()
-                .block_error("battery", "failed to parse  energy_now")?
-        } else {
-            0
-        };
-
-        let energy_full = if file_exists(&format!("{}energy_full", self.device_path)) {
-            read_file("battery", &format!("{}energy_full", self.device_path))?
-                .parse::<u64>()
-                .block_error("battery", "failed to parse  energy_full")?
-        } else {
-            0
-        };
-
-        let power_now = if file_exists(&format!("{}power_now", self.device_path)) {
-            read_file("battery", &format!("{}power_now", self.device_path))?
-                .parse::<u64>()
-                .block_error("battery", "failed to parse current voltage")?
-        } else {
-            0
-        };
-
-        let (hours, minutes) = if power_now > 0 && energy_now > 0 {
-            if state == "Discharging" {
-                let h = (energy_now as f64 / power_now as f64) as u64;
-                let m = (((energy_now as f64 / power_now as f64) - h as f64) * 60.0) as u64;
-                (h, m)
-            } else if state == "Charging" {
-                let h = ((energy_full as f64 - energy_now as f64) / power_now as f64) as u64;
-                let m = ((((energy_full as f64 - energy_now as f64) / power_now as f64) - h as f64) * 60.0) as u64;
-                (h, m)
-            } else {
-                (0, 0)
-            }
-        } else {
-            (0, 0)
-        };
+        let time_remaining = self.device.time_remaining()?;
+        let hours = time_remaining / 60;
+        let minutes = time_remaining % 60;
 
         // Don't need to display a percentage when the battery is full
         if current_percentage != 100 && state != "Full" {
@@ -191,13 +239,6 @@ impl Block for Battery {
             "Full" => "bat_full",
             "Discharging" => "bat_discharging",
             "Charging" => "bat_charging",
-            "Unknown" => {
-                if energy_now >= energy_full {
-                    "bat_full"
-                } else {
-                    "bat"
-                }
-            }
             _ => "bat",
         });
 
