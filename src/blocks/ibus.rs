18afeb6c4ceea586697ee2a8270cb56d60757003
Jason <jason@kayoway.com>
2018-09-26 13:05:24 +0900

    use non rustdoc comment style

--- src/blocks/ibus.rs
+++ src/blocks/ibus.rs
@@ -29,8 +29,8 @@ pub struct IBus {
 #[derive(Deserialize, Debug, Default, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct IBusConfig {
-    /// TODO: Implement this.
-    /// Set to display engine name as the two letter country abbreviation, e.g. "jp".
+    // TODO: Implement this.
+    // Set to display engine name as the two letter country abbreviation, e.g. "jp".
     #[serde(default = "IBusConfig::default_abbreviate")]
     pub as_icon: bool,
 }
@@ -57,12 +57,12 @@ impl ConfigBlock for IBus {
         let info: arg::Variant<Box<arg::RefArg>> = p.get("org.freedesktop.IBus", "GlobalEngine")
             .block_error("ibus", "Couldn't get ibus address")?;
 
-        /// `info` should contain something containing an array with the contents as such:
-        /// [name, longname, description, language, license, author, icon, layout, layout_variant, layout_option, rank, hotkeys, symbol, setup, version, textdomain, icon_prop_key]
-        /// Refer to: https://github.com/ibus/ibus/blob/7cef5bf572596361bc502e8fa917569676a80372/src/ibusenginedesc.c
-        /// e.g.                   name           longname        description     language
-        /// ["IBusEngineDesc", {}, "xkb:us::eng", "English (US)", "English (US)", "en", "GPL", "Peng Huang <shawn.p.huang@gmail.com>", "ibus-keyboard", "us", 99, "", "", "", "", "", "", "", ""]
-        ///                         ↑ We will use this element (name) as it is what GlobalEngineChanged signal returns.
+        // `info` should contain something containing an array with the contents as such:
+        // [name, longname, description, language, license, author, icon, layout, layout_variant, layout_option, rank, hotkeys, symbol, setup, version, textdomain, icon_prop_key]
+        // Refer to: https://github.com/ibus/ibus/blob/7cef5bf572596361bc502e8fa917569676a80372/src/ibusenginedesc.c
+        // e.g.                   name           longname        description     language
+        // ["IBusEngineDesc", {}, "xkb:us::eng", "English (US)", "English (US)", "en", "GPL", "Peng Huang <shawn.p.huang@gmail.com>", "ibus-keyboard", "us", 99, "", "", "", "", "", "", "", ""]
+        //                         ↑ We will use this element (name) as it is what GlobalEngineChanged signal returns.
         let current = info.0.as_iter().unwrap().nth(2).unwrap().as_str().unwrap_or("??");
         let engine_original = Arc::new(Mutex::new(String::from(current)));
 
@@ -77,7 +77,7 @@ impl ConfigBlock for IBus {
                     if let Some(engine_name) = parse_msg(&ci) {
                             let mut engine = engine_original.lock().unwrap();
                             *engine = engine_name.to_string();
-                            /// Tell block to update now.
+                            // Tell block to update now.
                             send.send(Task {
                                 id: id.clone(),
                                 update_time: Instant::now(),
@@ -100,7 +100,7 @@ impl Block for IBus {
         &self.id
     }
 
-    /// Updates the internal state of the block.
+    // Updates the internal state of the block.
     fn update(&mut self) -> Result<Option<Duration>> {
         let string = (*self.engine
             .lock()
@@ -110,14 +110,14 @@ impl Block for IBus {
         Ok(None)
     }
 
-    /// Returns the view of the block, comprised of widgets.
+    // Returns the view of the block, comprised of widgets.
     fn view(&self) -> Vec<&I3BarWidget> {
         vec![&self.text]
     }
 
-    /// This function is called on every block for every click.
-    /// TODO: Filter events by using the event.name property,
-    /// and use to switch between input engines?
+    // This function is called on every block for every click.
+    // TODO: Filter events by using the event.name property,
+    // and use to switch between input engines?
     fn click(&mut self, _: &I3BarEvent) -> Result<()> {
         Ok(())
     }
@@ -131,27 +131,27 @@ fn parse_msg(ci: &ConnectionItem) -> Option<&str> {
     engine
 }
 
-/// Gets the address being used by the currently running ibus daemon.
-/// 
-/// By default ibus will write the address to `$XDG_CONFIG_HOME/ibus/bus/aaa-bbb-ccc`
-/// where aaa = dbus machine id, usually found at /etc/machine-id
-///       bbb = hostname - seems to be "unix" in most cases [see L99 of reference]
-///       ccc = display number from $DISPLAY
-/// Refer to: https://github.com/ibus/ibus/blob/7cef5bf572596361bc502e8fa917569676a80372/src/ibusshare.c
-///
-/// Example file contents:
-/// ```
-/// # This file is created by ibus-daemon, please do not modify it
-/// IBUS_ADDRESS=unix:abstract=/tmp/dbus-8EeieDfT,guid=7542d73dce451c2461a044e24bc131f4
-/// IBUS_DAEMON_PID=11140
-/// ```
+// Gets the address being used by the currently running ibus daemon.
+// 
+// By default ibus will write the address to `$XDG_CONFIG_HOME/ibus/bus/aaa-bbb-ccc`
+// where aaa = dbus machine id, usually found at /etc/machine-id
+//       bbb = hostname - seems to be "unix" in most cases [see L99 of reference]
+//       ccc = display number from $DISPLAY
+// Refer to: https://github.com/ibus/ibus/blob/7cef5bf572596361bc502e8fa917569676a80372/src/ibusshare.c
+//
+// Example file contents:
+// ```
+// # This file is created by ibus-daemon, please do not modify it
+// IBUS_ADDRESS=unix:abstract=/tmp/dbus-8EeieDfT,guid=7542d73dce451c2461a044e24bc131f4
+// IBUS_DAEMON_PID=11140
+// ```
 fn get_ibus_address() -> Result<String> {
-    /// TODO: Check IBUS_ADDRESS variable, as it seems it can be manually set too.
+    // TODO: Check IBUS_ADDRESS variable, as it seems it can be manually set too.
 
     let config_dir = env::var("XDG_CONFIG_HOME")
         .block_error("ibus", "Problem getting $XDG_CONFIG_HOME")?;
 
-    /// TODO: Check /var/lib/dbus/machine-id if /etc/machine-id fails
+    // TODO: Check /var/lib/dbus/machine-id if /etc/machine-id fails
     let mut f = File::open("/etc/machine-id")
         .block_error("ibus", "Could not open /etc/machine-id")?;
     let mut machine_id = String::new();
@@ -159,10 +159,10 @@ fn get_ibus_address() -> Result<String> {
         .block_error("ibus", "Something went wrong reading /etc/machine-id")?;
     let machine_id = machine_id.trim();
 
-    /// On sway, $DISPLAY is only set by programs requiring xwayland, such as ibus (GTK2).
-    /// ibus-daemon can be autostarted by sway (via an entry in config file), however since
-    /// the bar is executed first, $DISPLAY will not yet be set at the time this code runs.
-    /// Hence on sway you will need to reload the bar once after login to get the block to work.
+    // On sway, $DISPLAY is only set by programs requiring xwayland, such as ibus (GTK2).
+    // ibus-daemon can be autostarted by sway (via an entry in config file), however since
+    // the bar is executed first, $DISPLAY will not yet be set at the time this code runs.
+    // Hence on sway you will need to reload the bar once after login to get the block to work.
     let display = env::var("DISPLAY")
         .block_error("ibus", "$DISPLAY not set. Try restarting block if on sway")?;
     let re = Regex::new(r"^:(\d{1})$").unwrap();
