2583eb47941309c78a46e89e86021490bba30a88
Alex Yosifov <sashomasho@gmail.com>
2019-10-12 17:10:59 +0300

    Reduce redundant code, and start using kbbd layout index to match setxkmap groups

--- src/blocks/keyboard_layout.rs
+++ src/blocks/keyboard_layout.rs
@@ -7,7 +7,6 @@ use crossbeam_channel::Sender;
 use dbus;
 use dbus::{Message, MsgHandlerResult, MsgHandlerType};
 use dbus::stdintf::org_freedesktop_dbus::Properties;
-use regex::Regex;
 use uuid::Uuid;
 
 use crate::blocks::{Block, ConfigBlock};
@@ -54,37 +53,41 @@ impl SetXkbMap {
     }
 }
 
+fn setxkbmap_layouts() -> Result<String> {
+    let output = Command::new("setxkbmap")
+        .args(&["-query"])
+        .output()
+        .block_error("keyboard_layout", "Failed to exectute setxkbmap.")
+        .and_then(|raw| {
+            String::from_utf8(raw.stdout).block_error("keyboard_layout", "Non-UTF8 input.")
+        })?;
+
+    // Find the "layout:    xxxx" entry.
+    let layout = output
+        .split('\n')
+        .filter(|line| line.starts_with("layout"))
+        .next()
+        .ok_or_else(|| {
+            BlockError(
+                "keyboard_layout".to_string(),
+                "Could not find the layout entry from setxkbmap.".to_string(),
+            )
+        })?
+        .split(char::is_whitespace)
+        .last();
+
+    match layout {
+        Some(layout) => Ok(layout.to_string()),
+        None => Err(BlockError(
+            "keyboard_layout".to_string(),
+            "Could not read the layout entry from setxkbmap.".to_string(),
+        )),
+    }
+}
+
 impl KeyboardLayoutMonitor for SetXkbMap {
     fn keyboard_layout(&self) -> Result<String> {
-        let output = Command::new("setxkbmap")
-            .args(&["-query"])
-            .output()
-            .block_error("keyboard_layout", "Failed to exectute setxkbmap.")
-            .and_then(|raw| {
-                String::from_utf8(raw.stdout).block_error("keyboard_layout", "Non-UTF8 input.")
-            })?;
-
-        // Find the "layout:    xxxx" entry.
-        let layout = output
-            .split('\n')
-            .filter(|line| line.starts_with("layout"))
-            .next()
-            .ok_or_else(|| {
-                BlockError(
-                    "keyboard_layout".to_string(),
-                    "Could not find the layout entry from setxkbmap.".to_string(),
-                )
-            })?
-            .split(char::is_whitespace)
-            .last();
-
-        match layout {
-            Some(layout) => Ok(layout.to_string()),
-            None => Err(BlockError(
-                "keyboard_layout".to_string(),
-                "Could not read the layout entry from setxkbmap.".to_string(),
-            )),
-        }
+        return setxkbmap_layouts();
     }
 
     fn must_poll(&self) -> bool {
@@ -155,49 +158,27 @@ impl KeyboardLayoutMonitor for LocaleBus {
 // also available in AUR and Debian) running, which enables per window keyboard layout,
 // really handy for dual-language typists who often change window focus
 pub struct KbdDaemonBus {
-    re_layout: Regex,
-    re_leds: Regex,
-
-    // indicates if xkblayout-state is installed
-    has_xkblayout: bool,
-    // indicates if setxkbmap && xset are installed
-    has_setxkbmap: bool,
-    // extracted from dbus message
-    kbdd_layout: Arc<Mutex<Option<String>>>,
+    // extracted from kbdd dbus message
+    kbdd_layout_id: Arc<Mutex<u32>>,
 }
 
 impl KbdDaemonBus {
     pub fn new() -> Result<Self> {
+        Command::new("setxkbmap")
+            .arg("-version")
+            .stdout(Stdio::piped())
+            .spawn()
+            .block_error("kbddaemonbus", "setxkbmap not found")?;
+
         // also verifies that kbdd daemon is registered in dbus
-        let layout = KbdDaemonBus::get_initial_layout()?;
+        let layout_id = KbdDaemonBus::get_initial_layout_id()?;
+
         Ok(KbdDaemonBus {
-            has_xkblayout: Command::new("xkblayout-state")
-                .stdout(Stdio::piped())
-                .args(&["print", "%s"])
-                .spawn()
-                .is_ok(),
-
-            has_setxkbmap: Command::new("setxkbmap")
-                .arg("-version")
-                .stdout(Stdio::piped())
-                .spawn()
-                .is_ok()
-                && Command::new("xset")
-                .arg("-version")
-                .stdout(Stdio::piped())
-                .spawn()
-                .is_ok(),
-
-            // example: layout:     us,bg:bas_phonetic
-            re_layout: Regex::new(r".*layout:.[ ]+(([a-zA-Z,:()_-]+,?)+).*").unwrap(),
-            // example: auto repeat:  on    key click percent:  0    LED mask:  00001000
-            re_leds: Regex::new(r".*LED mask:[ ]+[0-9]{4}([01])[0-9]{3}.*").unwrap(),
-
-            kbdd_layout: Arc::new(Mutex::new(Some(layout))),
+            kbdd_layout_id: Arc::new(Mutex::new(layout_id)),
         })
     }
 
-    fn get_initial_layout() -> Result<String> {
+    fn get_initial_layout_id() -> Result<u32> {
         let c = dbus::Connection::get_private(dbus::BusType::Session)
             .block_error("kbddaemonbus", "can't connect to dbus")?;
 
@@ -213,117 +194,42 @@ impl KbdDaemonBus {
         let current_layout_id: u32 = repl_msg.get1().ok_or("")
             .block_error("kbddaemonbus", "dbus kbdd response error")?;
 
-        let send_msg = Message::new_method_call(
-            "ru.gentoo.KbddService",
-            "/ru/gentoo/KbddService",
-            "ru.gentoo.kbdd",
-            "getLayoutName")
-            .block_error("kbddaemonbus", "Create get-layout-name message failure")?
-            .append(current_layout_id);
-
-        let repl_msg = c.send_with_reply_and_block(send_msg, 5000)
-            .block_error("kbddaemonbus", "dbus send message error")?;
-
-        let layout_name: &str = repl_msg.get1().ok_or("")
-            .block_error("kbddaemonbus", "Error obtaining current layout name")?;
-
-        Ok(layout_name.split(char::is_whitespace).nth(0).unwrap().to_string())
-    }
-}
-
-impl KbdDaemonBus {
-    // load current keyboard layout using xkblayout-state
-    fn xkblayout_state_layout(&self) -> Result<String> {
-        let output = Command::new("xkblayout-state")
-            .args(&["print", "%s"])
-            .output()
-            .block_error("kbddaemonbus", "Failed to execute xkblayout-state")?
-            .stdout;
-        let result_str = String::from_utf8(output)
-            .block_error("kbddaemonbus", "Non-UTF8 input from xkblayout-state")?;
-        Ok(result_str)
-    }
-
-    // Load current keyboard layout using setxkbmap and xset,
-    // the former shows enabled keyboard layouts, and the latter is
-    // used to obtain the current one.
-    // This is going to work for 2 layouts only, maybe if there are more
-    // it should fallback to the last variant (self.has_setxkbmap=false) and
-    // use kbdd bus value (we already have it, but it's long literal, e.g. 'English',
-    // which I don't really like, since it consumes a lot of space)
-    fn setxkmbmap_layout(&self) -> Result<String> {
-        //find enabled layouts
-        let output = Command::new("setxkbmap")
-            .arg("-query")
-            .output()
-            .block_error("kbddaemonbus", "Failed to execute setxkbmap")?
-            .stdout;
-
-        let result_str = String::from_utf8(output)
-            .block_error("kbddaemonbus", "Non-UTF8 input from setxkbmap")?;
-
-        let layout_match = result_str.lines()
-            .find_map(|line| {
-                self.re_layout.captures(line)
-            }).block_error("kbddaemonbus", "Failed to find current layouts")?
-            .get(1).unwrap(); //we are sure that it's there because captures would have failed
-        let layouts: Vec<&str> = layout_match.as_str().split(",").map(|s| s).collect();
-
-        // check the current layout by reading LED mask in xset output,
-        // 0000000 for the first, 0000100 for second,
-        // if there are more enabled we will show the second one :(
-        let output = Command::new("xset")
-            .arg("-q")
-            .output()
-            .block_error("kbddaemonbus", "Failed to execute xset")?
-            .stdout;
-
-        let result_str = String::from_utf8(output)
-            .block_error("kbddaemonbus", "Non-UTF8 input from xset")?;
-
-        let leds_indicator = result_str.lines()
-            .find_map(|line| {
-                self.re_leds.captures(line)
-            }).block_error("kbddaemonbus", "Failed to parse xset output")?
-            .get(1).unwrap()
-            .as_str()
-            .parse::<usize>()
-            .block_error("kbddaemonbus", "Failed to parse xset leds output")?;
-
-        let layout: &&str = layouts.get(leds_indicator).ok_or("no item?")
-            .block_error("kbddaemonbus", "Failed to find current layout")?;
-
-        Ok(layout.split(":").nth(0).unwrap().to_string()) //remove variant if present, e.g. bg:pas_phonetic
+        Ok(current_layout_id)
     }
 }
 
 impl KeyboardLayoutMonitor for KbdDaemonBus {
     fn keyboard_layout(&self) -> Result<String> {
-        if self.has_xkblayout {
-            self.xkblayout_state_layout()
-        } else if self.has_setxkbmap {
-            self.setxkmbmap_layout()
-        } else {
-            match &*self.kbdd_layout.lock().unwrap() {
-                Some(s) => Ok((&s).to_string()),
-                None => Err(BlockError(
-                    "kbddaemonbus".to_string(),
-                    "Failed to load layout from kbdd".to_owned())) //should never happen :)
-            }
+        let layouts_str = setxkbmap_layouts()?;
+        let idx = *self.kbdd_layout_id.lock().unwrap();
+
+        let split = layouts_str
+            .split(",")
+            .nth(idx as usize);
+
+        match split {
+            Some(s) => Ok(s.to_string()),
+
+            //'None' may happen only if keyboard map is being toggled (by window focus or keyboard)
+            //and keyboard layout replaced (by calling setxkmap) at almost the same time,
+            //frankly I can't reproduce this without thread sleep in monitor function, so instead
+            //of block_error I think showing all layouts will be better until the next
+            //toggling happens
+            None=> Ok(layouts_str)
         }
     }
 
     fn must_poll(&self) -> bool { false }
 
-    // Monitor KbdDaemon property changes in a separate thread and send updates
+    // Monitor KbdDaemon 'layoutChanged' property in a separate thread and send updates
     // via the `update_request` channel.
     fn monitor(&self, id: String, update_request: Sender<Task>) {
-        let arc = Arc::clone(&self.kbdd_layout);
+        let arc = Arc::clone(&self.kbdd_layout_id);
         thread::spawn(move || {
             let c = dbus::Connection::get_private(dbus::BusType::Session).unwrap();
             c.add_match(
                 "interface='ru.gentoo.kbdd',\
-                member='layoutNameChanged',\
+                member='layoutChanged',\
                 path='/ru/gentoo/KbddService'",
             ).expect("Failed to add D-Bus match rule, is kbdd started?");
 
