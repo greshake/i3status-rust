848b3317d0b478d66742eda0f63a21218f58b298
Aaron Jacobs <atheriel@gmail.com>
2020-02-08 16:29:51 -0500

    travis: Adds a check to see if the manpage has been updated.
    The check does not yet fail the build because different versions of
    pandoc produce slightly different output.

--- man/generate.sh
+++ man/generate.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+if [ -z $1 ]; then
+    OUT=man/i3status-rs.1
+else
+    OUT=$1
+fi
+
 pandoc -o man/blocks.1 -t man blocks.md
 pandoc -o man/themes.1 -t man --base-header-level=2 themes.md
 
@@ -13,6 +19,6 @@ sed -i '1i .SH BLOCKS\n' man/blocks.1
 sed -i '1i .SH THEMES\n' man/themes.1
 
 # Stich together the final manpage.
-cat man/_preface.1 man/blocks.1 man/themes.1 man/_postface.1 > man/i3status-rs.1
+cat man/_preface.1 man/blocks.1 man/themes.1 man/_postface.1 > $OUT
 
 rm man/blocks.1 man/themes.1
