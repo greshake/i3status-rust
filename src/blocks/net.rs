1354f9a536f92c2f9ef572ad8d5eea2bd64f88db
Dieter Plaetinck <dieter@grafana.com>
2020-09-01 16:22:10 +0200

    apply PR feedback from Stuart / themadprofessor

--- src/blocks/net.rs
+++ src/blocks/net.rs
@@ -991,18 +991,14 @@ fn get_iw_ssid(dev: &NetworkDevice) -> Result<Option<String>> {
     }
 
     let raw = raw.unwrap();
-    let bytes = raw.stdout.split(|c| *c == b'\n').next().unwrap();
-    let cap = IW_SSID_REGEX.captures(bytes);
-    if cap.is_none() {
-        return Ok(None);
-    }
-    let match_obj = cap.unwrap().get(1);
-    if match_obj.is_none() {
-        return Ok(None);
-    }
+    let result = raw
+        .stdout
+        .split(|c| *c == b'\n')
+        .filter_map(|x| IW_SSID_REGEX.captures_iter(x).next())
+        .filter_map(|x| x.get(1))
+        .next();
 
-    let caps = match_obj.unwrap().as_bytes();
-    maybe_ssid_convert(Some(caps))
+    maybe_ssid_convert(result.map(|x| x.as_bytes()))
 }
 
 #[inline]
