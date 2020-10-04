1c6de29560110013e760ebd022133e6abaa09e3a
grewka <grewka@grewka.grewka>
2020-10-04 09:42:25 +0200

    Add C-bindings for SIGRTMIN and MAX
    Remove the hardcoded values for SIGRTMIN and SIGRTMAX and replace them
    with C bindings. This fixes the bug that could potentially occur if the
    kernel does not have SIGRTMIN = 34 and SIGRTMAX = 64.
    
    The C bindings should be replaced with libc or nix crate bindings when
    these become avaliable.

--- src/signals.rs
+++ src/signals.rs
@@ -1,47 +1,65 @@
 use crate::errors::*;
 use crossbeam_channel::Sender;
 use std::thread;
-/// These are the SIGRTMIN and SIGRTMAX values used to determine the allowed range of signal values
-//FIXME currently these are hardcoded and tested on my system, I am not sure how to bring these in
-//dynamically from the host OS, as such they may vary with OS
-const SIGMIN: i32 = 34;
-const SIGMAX: i32 = 64;
 
 /// Starts a thread that listens for provided signals and sends these on the provided channel
 pub fn process_signals(sender: Sender<i32>) {
     thread::Builder::new()
         .name("signals".into())
-        .spawn(move || loop {
-            let signals =
-                signal_hook::iterator::Signals::new(&(SIGMIN..SIGMAX).collect::<Vec<_>>()).unwrap();
-            for sig in signals.forever() {
-                sender.send(sig).unwrap();
+        .spawn(move || {
+            let sigmin;
+            let sigmax;
+            unsafe {
+                sigmin = __libc_current_sigrtmin();
+                sigmax = __libc_current_sigrtmax();
+            }
+            loop {
+                let signals =
+                    signal_hook::iterator::Signals::new(&(sigmin..sigmax).collect::<Vec<_>>())
+                        .unwrap();
+                for sig in signals.forever() {
+                    sender.send(sig).unwrap();
+                }
             }
         })
         .unwrap();
 }
 
 pub fn convert_to_valid_signal(signal: i32) -> Result<i32> {
-    if signal < 0 || signal > SIGMAX - SIGMIN {
+    let sigmin;
+    let sigmax;
+    unsafe {
+        sigmin = __libc_current_sigrtmin();
+        sigmax = __libc_current_sigrtmax();
+    }
+    if signal < 0 || signal > sigmax - sigmin {
         //NOTE If some important information is encoded in the third field of this error this might
         //need to be added
         return Err(Error::ConfigurationError(
             format!(
             "A provided signal was out of bounds. An allowed signal needs to be between {} and {}",
             0,
-            SIGMAX - SIGMIN
+            sigmax - sigmin
         ),
             (
                 format!(
                     "Provided signal is {} which is not between {} and {}",
                     signal,
                     0,
-                    SIGMAX - SIGMIN
+                    sigmax - sigmin
                 ),
                 String::new(),
             ),
         ));
     } else {
-        return Ok(signal + SIGMIN);
+        return Ok(signal + sigmin);
     }
+}
+
+//TODO when libc exposes this through their library and even better when the nix crate does we
+//should be using that binding rather than a C-binding.
+///C bindings to SIGMIN and SIGMAX values
+extern "C" {
+    fn __libc_current_sigrtmin() -> i32;
+    fn __libc_current_sigrtmax() -> i32;
 }
