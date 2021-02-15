a765f6cee38262c47012d7cdf9ba5e0ad1faaee9
Jason Nader <jason.nader@protonmail.com>
2021-02-15 18:19:57 +0900

    Backport id fix

--- src/main.rs
+++ src/main.rs
@@ -110,7 +110,7 @@ fn main() {
             ::std::process::exit(1);
         }
 
-        let error_widget = TextWidget::new(Default::default(), 9999999999)
+        let error_widget = TextWidget::new(Default::default(), 0)
             .with_state(State::Critical)
             .with_text(&format!("{:?}", error));
         let error_rendered = error_widget.get_rendered();
