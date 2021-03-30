47bcdff5abdd3bd503c16e7290425914785648c6
Jason <ammgws@users.noreply.github.com>
2021-03-30 22:48:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# Use this script when installing via `cargo` in order to be able to use the default icons/themes
 mkdir -p ~/.local/share/i3status-rust
 cp -r files/* ~/.local/share/i3status-rust/
