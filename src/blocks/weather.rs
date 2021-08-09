e3ac160da34cb5737f9edfc3d238c86c198248be
Ronak Buch <ronakbuch@gmail.com>
2021-08-09 19:52:54 -0400

    Weather: Remove duplicate degree symbol from default format
    A degree symbol is added by the Unit formatter already, so there should
    not be an additional one added in the default format string.

--- src/blocks/weather.rs
+++ src/blocks/weather.rs
@@ -338,7 +338,7 @@ impl ConfigBlock for Weather {
             weather: TextWidget::new(id, 0, shared_config),
             format: block_config
                 .format
-                .with_default("{weather} {temp}\u{00b0}")?,
+                .with_default("{weather} {temp}")?,
             weather_keys: HashMap::new(),
             service: block_config.service,
             update_interval: block_config.interval,
