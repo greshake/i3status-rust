e8a79c293cb1b440b4ecb750528b09967ab33c8b
Max Verevkin <maxxverrr@gmail.com>
2022-04-16 11:22:54 +0300

    focused_window: add river support

--- src/blocks/focused_window.rs
+++ src/blocks/focused_window.rs
@@ -11,12 +11,13 @@
 //! ----|--------|----------|--------
 //! `format` | A string to customise the output of this block. See below for available placeholders. | No | <code>"$title.rot-str(15)&vert;"</code>
 //! `autohide` | Whether to hide the block when no title is available | No | `true`
+//! `driver` | Which driver to use. Available values: `sway_ipc` - for `i3` and `sway`, `ristate` - for `river` (note that [`ristate`](https://gitlab.com/snakedye/ristate) binary must be in the `PATH`, `auto` - try to automatically guess which driver to use. | No | `"auto"`
 //!
-//! Placeholder     | Value                                     | Type | Unit
-//! ----------------|-------------------------------------------|------|-----
-//! `title`         | Window's titile (may be absent)           | Text | -
-//! `marks`         | Window's marks                            | Text | -
-//! `visible_marks` | Window's marks that do not start with `_` | Text | -
+//! Placeholder     | Value                                                                 | Type | Unit
+//! ----------------|-----------------------------------------------------------------------|------|-----
+//! `title`         | Window's titile (may be absent)                                       | Text | -
+//! `marks`         | Window's marks (present only with sway/i3)                            | Text | -
+//! `visible_marks` | Window's marks that do not start with `_` (present only with sway/i3) | Text | -
 //!
 //! # Example
 //!
@@ -31,11 +32,23 @@
 use super::prelude::*;
 use swayipc_async::{Connection, Event, EventType, WindowChange, WorkspaceChange};
 
+use std::process::Stdio;
+use tokio::{io::BufReader, process::Command};
+
 #[derive(Deserialize, Debug)]
 #[serde(deny_unknown_fields, default)]
 struct FocusedWindowConfig {
     format: FormatConfig,
     autohide: bool,
+    driver: Driver,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+enum Driver {
+    Auto,
+    SwayIpc,
+    Ristate,
 }
 
 impl Default for FocusedWindowConfig {
@@ -43,6 +56,7 @@ impl Default for FocusedWindowConfig {
         Self {
             format: Default::default(),
             autohide: true,
+            driver: Driver::Auto,
         }
     }
 }
@@ -51,19 +65,34 @@ pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
     let config = FocusedWindowConfig::deserialize(config).config_error()?;
     api.set_format(config.format.with_default("$title.rot-str(15)|")?);
 
-    let mut title: Option<String> = None;
-    let mut marks = Vec::new();
+    match config.driver {
+        Driver::Auto => match Connection::new().await {
+            Ok(conn) => with_sway_ipc(conn, config.autohide, &mut api).await,
+            Err(_) => with_ristate(config.autohide, &mut api).await,
+        },
+        Driver::SwayIpc => {
+            with_sway_ipc(
+                Connection::new()
+                    .await
+                    .error("failed to open connection with swayipc")?,
+                config.autohide,
+                &mut api,
+            )
+            .await
+        }
+        Driver::Ristate => with_ristate(config.autohide, &mut api).await,
+    }
+}
 
-    let conn = Connection::new()
-        .await
-        .error("failed to open connection with swayipc")?;
+async fn with_sway_ipc(conn: Connection, autohide: bool, api: &mut CommonApi) -> Result<()> {
+    let mut title = String::new();
+    let mut marks = Vec::new();
 
     let mut events = conn
         .subscribe(&[EventType::Window, EventType::Workspace])
         .await
         .error("could not subscribe to window events")?;
 
-    // Main loop
     loop {
         let event = events
             .next()
@@ -78,27 +107,33 @@ pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
                     true
                 }
                 WindowChange::Focus => {
-                    title = e.container.name.as_ref().map(|t| t.into());
+                    title.clear();
+                    if let Some(new_title) = &e.container.name {
+                        title.push_str(new_title);
+                    }
                     marks = e.container.marks;
                     true
                 }
                 WindowChange::Title => {
                     if e.container.focused {
-                        title = e.container.name.as_ref().map(|t| t.into());
+                        title.clear();
+                        if let Some(new_title) = &e.container.name {
+                            title.push_str(new_title);
+                        }
                         true
                     } else {
                         false
                     }
                 }
                 WindowChange::Close => {
-                    title = None;
+                    title.clear();
                     marks.clear();
                     true
                 }
                 _ => false,
             },
             Event::Workspace(e) if e.change == WorkspaceChange::Init => {
-                title = None;
+                title.clear();
                 marks.clear();
                 true
             }
@@ -106,15 +141,12 @@ pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
         };
 
         if updated {
-            if title.is_some() || !config.autohide {
-                let mut values = map! {
+            if !title.is_empty() || !autohide {
+                api.set_values(map! {
+                    "title" => Value::text(title.clone()),
                     "marks" => Value::text(marks.iter().map(|m| format!("[{m}]")).collect()),
                     "visible_marks" => Value::text(marks.iter().filter(|m| !m.starts_with('_')).map(|m| format!("[{m}]")).collect()),
-                };
-                title
-                    .clone()
-                    .map(|t| values.insert("title".into(), Value::text(t)));
-                api.set_values(values);
+                });
                 api.show();
             } else {
                 api.hide();
@@ -122,4 +154,43 @@ pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
             api.flush().await?;
         }
     }
+}
+
+async fn with_ristate(autohide: bool, api: &mut CommonApi) -> Result<()> {
+    #[derive(Deserialize, Debug)]
+    struct RistateOuput {
+        title: String,
+    }
+
+    let mut ristate = Command::new("ristate")
+        .arg("-w")
+        .stdout(Stdio::piped())
+        .spawn()
+        .error("failed to run ristate")?;
+    let mut stream = BufReader::new(ristate.stdout.take().unwrap()).lines();
+
+    tokio::spawn(async move {
+        let _ = ristate.wait().await;
+    });
+
+    while let Some(line) = stream
+        .next_line()
+        .await
+        .error("error reading line from ristate")?
+    {
+        let title = serde_json::from_str::<RistateOuput>(&line)
+            .error("ristate produced invalid json")?
+            .title;
+        if !title.is_empty() || !autohide {
+            api.set_values(map! {
+                "title" => Value::text(title.clone()),
+            });
+            api.show();
+        } else {
+            api.hide();
+        }
+        api.flush().await?;
+    }
+
+    Err(Error::new("ristate exited unexpectedly"))
 }
