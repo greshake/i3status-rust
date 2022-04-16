71f5c88770714d843f2af9bd92a487db0703a280
MaxVerevkin <34583604+MaxVerevkin@users.noreply.github.com>
2022-04-16 08:26:26 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@e8a79c293cb1b440b4ecb750528b09967ab33c8b 🚀

--- i3status_rs/blocks/focused_window/sidebar-items.js
+++ i3status_rs/blocks/focused_window/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["run",""]],"struct":[["FocusedWindowConfig",""]]});
+initSidebarItems({"enum":[["Driver",""]],"fn":[["run",""],["with_ristate",""],["with_sway_ipc",""]],"struct":[["FocusedWindowConfig",""]]});
