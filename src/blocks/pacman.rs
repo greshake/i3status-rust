6025a628ae758a2d8d86cec34d555c23b022a655
Jason Nader <ammgws@users.noreply.github.com>
2022-07-09 17:17:59 +0900

    Update pacman hook dir
    Should be using /etc/ not /usr

--- src/blocks/pacman.rs
+++ src/blocks/pacman.rs
@@ -4,7 +4,7 @@
 //!
 //! Tip: You can grab the list of available updates using `fakeroot pacman -Qu --dbpath /tmp/checkup-db-yourusername/`. If you have the CHECKUPDATES_DB env var set on your system then substitute that dir instead of /tmp/checkup-db-yourusername.
 //!
-//! Tip: On Arch Linux you can setup a `pacman` hook to signal i3status-rs to update after packages have been upgraded, so you won't have stale info in your pacman block. Create `/usr/share/libalpm/hooks/i3status.hook` with the below contents:
+//! Tip: On Arch Linux you can setup a `pacman` hook to signal i3status-rs to update after packages have been upgraded, so you won't have stale info in your pacman block. Create `/etc/pacman.d/hooks/i3status-rust.hook` with the below contents:
 //!
 //! Note: `pikaur` may hang the whole block if there is no internet connectivity. In that case, try a different AUR helper.
 //! ```ini
