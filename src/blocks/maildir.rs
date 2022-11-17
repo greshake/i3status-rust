greshake/i3status-rust

commit d8732d6f7682fe83617db0ebd51faf101a0a7085

refactor: avoid toml::Value manipulation (#1652)

src/blocks/maildir.rs
@@ -40,8 +40,8 @@ use super::prelude::*;
 use maildir::Maildir;
 
 #[derive(Deserialize, Debug, SmartDefault)]
-#[serde(deny_unknown_fields, default)]
-struct MaildirConfig {
+#[serde(default)]
+pub struct Config {
     format: FormatConfig,
     #[default(5.into())]
     interval: Seconds,
@@ -54,8 +54,7 @@ struct MaildirConfig {
     display_type: MailType,
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let mut config = MaildirConfig::deserialize(config).config_error()?;
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let mut widget = Widget::new().with_format(config.format.with_default(" $icon $status ")?);
 
     for inbox in &mut config.inboxes {

src/blocks.rs
@@ -2,20 +2,14 @@
 
 pub mod prelude;
 
-use crate::formatting::config::Config as FormatConfig;
-use crate::themes::ThemeOverrides;
 use crate::BoxedFuture;
 use futures::future::FutureExt;
-use serde::de::{self, Deserializer};
 use serde::Deserialize;
 use tokio::sync::mpsc;
-use toml::value::Table;
 
-use std::collections::HashMap;
 use std::future::Future;
 use std::time::Duration;
 
-use crate::click::ClickHandler;
 use crate::config::SharedConfig;
 use crate::errors::*;
 use crate::protocol::i3bar_event::I3BarEvent;
@@ -31,65 +25,42 @@ macro_rules! define_blocks {
             pub mod $block;
         )*
 
-        #[derive(Debug, Clone, Copy)]
-        pub enum BlockType {
+        #[derive(Debug, Deserialize)]
+        #[serde(tag = "block")]
+        #[serde(deny_unknown_fields)]
+        pub enum BlockConfig {
             $(
                 $(#[cfg($attr)])?
                 #[allow(non_camel_case_types)]
-                $block,
+                $block {
+                    #[serde(flatten)]
+                    config: $block::Config,
+                },
             )*
         }
 
-        impl BlockType {
-            pub fn run(self, config: toml::Value, api: CommonApi) -> BlockFuture {
+        impl BlockConfig {
+            pub fn name(&self) -> &'static str {
+                match self {
+                    $(
+                        $(#[cfg($attr)])?
+                        Self::$block { .. } => stringify!($block),
+                    )*
+                }
+            }
+
+            pub fn run(self, api: CommonApi) -> BlockFuture {
                 let id = api.id;
                 match self {
                     $(
                         $(#[cfg($attr)])?
-                        Self::$block => {
-                            $block::run(config, api).map(move |e| e.in_block(self, id)).boxed_local()
+                        Self::$block { config } => {
+                            $block::run(config, api).map(move |e| e.in_block(stringify!($block), id)).boxed_local()
                         }
                     )*
                 }
             }
         }
-
-        impl<'de> Deserialize<'de> for BlockType {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-            where
-                D: Deserializer<'de>,
-            {
-                struct Visitor;
-
-                impl<'de> de::Visitor<'de> for Visitor {
-                    type Value = BlockType;
-
-                    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                        formatter.write_str("a block name")
-                    }
-
-                    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-                    where
-                        E: de::Error,
-                    {
-                        match v {
-                            $(
-                            $(#[cfg($attr)])?
-                            stringify!($block) => Ok(BlockType::$block),
-                            $(
-                            #[cfg(not($attr))]
-                            stringify!($block) => Err(E::custom(format!("Block '{}' has to be enabled at the compile time", stringify!($block)))),
-                            )?
-                            )*
-                            unknown => Err(E::custom(format!("Unknown block '{unknown}'")))
-                        }
-                    }
-                }
-
-                deserializer.deserialize_str(Visitor)
-            }
-        }
-
     };
 }
 
@@ -279,62 +250,5 @@ impl CommonApi {
                 }
             }
         }
-    }
-}
-
-#[derive(Deserialize, Debug)]
-pub struct CommonConfig {
-    pub block: BlockType,
-
-    #[serde(default)]
-    pub click: ClickHandler,
-    #[serde(default)]
-    pub signal: Option<i32>,
-    #[serde(default)]
-    pub icons_format: Option<String>,
-    #[serde(default)]
-    pub theme_overrides: Option<ThemeOverrides>,
-    #[serde(default)]
-    pub icons_overrides: Option<HashMap<String, String>>,
-
-    #[serde(default = "CommonConfig::default_error_interval")]
-    pub error_interval: u64,
-    #[serde(default)]
-    pub error_format: FormatConfig,
-    #[serde(default)]
-    pub error_fullscreen_format: FormatConfig,
-
-    #[serde(default)]
-    pub if_command: Option<String>,
-}
-
-impl CommonConfig {
-    fn default_error_interval() -> u64 {
-        5
-    }
-
-    pub fn new(from: &mut toml::Value) -> Result<Self> {
-        const FIELDS: &[&str] = &[
-            "block",
-            "click",
-            "signal",
-            "icons_format",
-            "theme_overrides",
-            "icons_overrides",
-            "error_interval",
-            "error_format",
-            "error_fullscreen_format",
-            "if_command",
-        ];
-        let mut common_table = Table::new();
-        if let Some(table) = from.as_table_mut() {
-            for &field in FIELDS {
-                if let Some(it) = table.remove(field) {
-                    common_table.insert(field.to_string(), it);
-                }
-            }
-        }
-        let common_value: toml::Value = common_table.into();
-        CommonConfig::deserialize(common_value).config_error()
     }
 }

src/blocks/apt.rs
@@ -52,8 +52,8 @@ use tokio::process::Command;
 use super::prelude::*;
 
 #[derive(Deserialize, Debug, SmartDefault)]
-#[serde(deny_unknown_fields, default)]
-struct AptConfig {
+#[serde(default)]
+pub struct Config {
     #[default(600.into())]
     interval: Seconds,
     format: FormatConfig,
@@ -63,8 +63,7 @@ struct AptConfig {
     critical_updates_regex: Option<String>,
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let config = AptConfig::deserialize(config).config_error()?;
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let mut widget = Widget::new();
 
     let format = config.format.with_default(" $icon $count.eng(1) ")?;

src/blocks/backlight.rs
@@ -110,8 +110,8 @@ const BACKLIGHT_ICONS: &[&str] = &[
 ];
 
 #[derive(Deserialize, Debug, SmartDefault)]
-#[serde(deny_unknown_fields, default)]
-struct BacklightConfig {
+#[serde(default)]
+pub struct Config {
     device: Option<String>,
     format: FormatConfig,
     #[default(5)]
@@ -126,8 +126,7 @@ struct BacklightConfig {
     invert_icons: bool,
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let config = BacklightConfig::deserialize(config).config_error()?;
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let mut widget = Widget::new().with_format(config.format.with_default(" $icon $brightness ")?);
 
     let mut cycle = config

src/blocks/battery.rs
@@ -74,8 +74,8 @@ mod upower;
 // make_log_macro!(debug, "battery");
 
 #[derive(Deserialize, Debug, SmartDefault)]
-#[serde(deny_unknown_fields, default)]
-struct BatteryConfig {
+#[serde(default)]
+pub struct Config {
     device: Option<String>,
     driver: BatteryDriver,
     #[default(10.into())]
@@ -107,8 +107,7 @@ enum BatteryDriver {
     Upower,
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let config = BatteryConfig::deserialize(config).config_error()?;
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let format = config.format.with_default(" $icon $percentage ")?;
     let format_full = config.full_format.with_default(" $icon ")?;
     let format_empty = config.empty_format.with_default(" $icon ")?;

src/blocks/bluetooth.rs
@@ -49,8 +49,7 @@ use zbus::fdo::{ObjectManagerProxy, PropertiesProxy};
 make_log_macro!(debug, "bluetooth");
 
 #[derive(Deserialize, Debug)]
-#[serde(deny_unknown_fields)]
-struct BluetoothConfig {
+pub struct Config {
     mac: String,
     #[serde(default)]
     adapter_mac: Option<String>,
@@ -60,8 +59,7 @@ struct BluetoothConfig {
     disconnected_format: FormatConfig,
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let config = BluetoothConfig::deserialize(config).config_error()?;
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let format = config.format.with_default(" $icon $name{ $percentage|} ")?;
     let disconnected_format = config
         .disconnected_format

src/blocks/cpu.rs
@@ -45,17 +45,15 @@ const CPU_BOOST_PATH: &str = "/sys/devices/system/cpu/cpufreq/boost";
 const CPU_NO_TURBO_PATH: &str = "/sys/devices/system/cpu/intel_pstate/no_turbo";
 
 #[derive(Deserialize, Debug, SmartDefault)]
-#[serde(deny_unknown_fields, default)]
-struct CpuConfig {
+#[serde(default)]
+pub struct Config {
     format: FormatConfig,
     format_alt: Option<FormatConfig>,
     #[default(5.into())]
     interval: Seconds,
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let config = CpuConfig::deserialize(config).config_error()?;
-
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let mut format = config.format.with_default(" $icon $utilization ")?;
     let mut format_alt = match config.format_alt {
         Some(f) => Some(f.with_default("")?),

src/blocks/custom.rs
@@ -98,8 +98,8 @@ use tokio::io::{self, AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 
 #[derive(Deserialize, Debug, SmartDefault)]
-#[serde(deny_unknown_fields, default)]
-struct CustomConfig {
+#[serde(default)]
+pub struct Config {
     format: FormatConfig,
     command: Option<String>,
     persistent: bool,
@@ -146,8 +146,7 @@ async fn update_bar(
     }
 }
 
-pub async fn run(config: toml::Value, mut api: CommonApi) -> Result<()> {
-    let config = CustomConfig::deserialize(config).config_error()?;
+pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
     let mut widget = Widget::new().with_format(
         config
             .format

commit 760f08df04396e8a55420628a61b0cfece9e48de

Fix change in maildir caused by #1652
```
cargo build --release --features 'pulseaudio maildir'
   Compiling i3status-rs v0.30.0 (/home/bmalyn/Documents/Code/rust/i3status-rust)
error[E0596]: cannot borrow `config.inboxes` as mutable, as `config` is not declared as mutable
  --> src/blocks/maildir.rs:60:18
   |
57 | pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
   |                  ------ help: consider changing this to be mutable: `mut config`
...
60 |     for inbox in &mut config.inboxes {
   |                  ^^^^^^^^^^^^^^^^^^^ cannot borrow as mutable

For more information about this error, try `rustc --explain E0596`.
error: could not compile `i3status-rs` due to previous error
```

src/blocks/maildir.rs
@@ -54,7 +54,7 @@ pub struct Config {
     display_type: MailType,
 }
 
-pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
+pub async fn run(mut config: Config, mut api: CommonApi) -> Result<()> {
     let mut widget = Widget::new().with_format(config.format.with_default(" $icon $status ")?);
 
     for inbox in &mut config.inboxes {