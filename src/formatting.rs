a5663039a4afd615df046940d04cde73d8f9c89c
Carlos Fernandez Sanz <carlos@sanz.dev>
2023-02-19 16:44:29 -0800

    Add width, w for constant width on formatter

--- src/formatting.rs
+++ src/formatting.rs
@@ -33,6 +33,8 @@
 //! -----------------------|---------------------------------------------------|-------------
 //! `min_width` or `min_w` | if text is shorter it will be padded using spaces | `0`
 //! `max_width` or `max_w` | if text is longer it will be truncated            | Infinity
+//! `width` or `w`         | Text will be exactly this length by padding or truncating as needed | min_width to
+//! max_width defaults
 //! `rot_interval`         | if text is longer than `max_width` it will be rotated every `rot_interval` seconds | `0.5`
 //!
 //! ## `eng` - Format numbers using engineering notation
