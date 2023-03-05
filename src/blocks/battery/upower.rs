6ed1b3ffc69421e007c25ca1508fc932cfb92164
Carlos Fernandez Sanz <carlos@sanz.dev>
2023-03-05 01:25:05 -0800

    battery: Filter battery selection by model (#1808)

--- src/blocks/battery/upower.rs
+++ src/blocks/battery/upower.rs
@@ -16,50 +16,64 @@ struct DeviceConnection {
 }
 
 impl DeviceConnection {
-    async fn new(dbus_conn: &Connection, device: &DeviceName) -> Result<Option<Self>> {
-        let device_proxy = if device.exact().map_or(true, |d| d == "DisplayDevice") {
-            DeviceProxy::builder(dbus_conn)
-                .path(DISPLAY_DEVICE_PATH)
-                .unwrap()
-                .build()
-                .await
-                .error("Failed to create DeviceProxy")?
-        } else {
-            let mut res = None;
-            for path in UPowerProxy::new(dbus_conn)
-                .await
-                .error("Failed to create UPowerProxy")?
-                .enumerate_devices()
-                .await
-                .error("Failed to retrieve UPower devices")?
-            {
-                let proxy = DeviceProxy::builder(dbus_conn)
-                    .path(path)
+    async fn new(
+        dbus_conn: &Connection,
+        device: &DeviceName,
+        expected_model: Option<&str>,
+    ) -> Result<Option<Self>> {
+        let device_proxy =
+            if device.exact().map_or(true, |d| d == "DisplayDevice") && expected_model.is_none() {
+                DeviceProxy::builder(dbus_conn)
+                    .path(DISPLAY_DEVICE_PATH)
                     .unwrap()
                     .build()
                     .await
-                    .error("Failed to create DeviceProxy")?;
-                // Verify device type
-                // https://upower.freedesktop.org/docs/Device.html#Device:Type
-                // consider any peripheral, UPS and internal battery
-                let device_type = proxy.type_().await.error("Failed to get device's type")?;
-                if device_type == 1 {
-                    continue;
-                }
-                let name = proxy
-                    .native_path()
+                    .error("Failed to create DeviceProxy")?
+            } else {
+                let mut res = None;
+                for path in UPowerProxy::new(dbus_conn)
                     .await
-                    .error("Failed to get device's native path")?;
-                if device.matches(&name) {
-                    res = Some(proxy);
-                    break;
+                    .error("Failed to create UPowerProxy")?
+                    .enumerate_devices()
+                    .await
+                    .error("Failed to retrieve UPower devices")?
+                {
+                    let proxy = DeviceProxy::builder(dbus_conn)
+                        .path(path)
+                        .unwrap()
+                        .build()
+                        .await
+                        .error("Failed to create DeviceProxy")?;
+
+                    // Filter by model if needed
+                    if let Some(expected_model) = &expected_model {
+                        if let Ok(device_model) = proxy.model().await {
+                            if !expected_model.eq(&device_model) {
+                                continue;
+                            }
+                        }
+                    }
+                    // Verify device type
+                    // https://upower.freedesktop.org/docs/Device.html#Device:Type
+                    // consider any peripheral, UPS and internal battery
+                    let device_type = proxy.type_().await.error("Failed to get device's type")?;
+                    if device_type == 1 {
+                        continue;
+                    }
+                    let name = proxy
+                        .native_path()
+                        .await
+                        .error("Failed to get device's native path")?;
+                    if device.matches(&name) {
+                        res = Some(proxy);
+                        break;
+                    }
                 }
-            }
-            match res {
-                Some(res) => res,
-                None => return Ok(None),
-            }
-        };
+                match res {
+                    Some(res) => res,
+                    None => return Ok(None),
+                }
+            };
 
         let changes = PropertiesProxy::builder(dbus_conn)
             .destination("org.freedesktop.UPower")
@@ -83,16 +97,17 @@ impl DeviceConnection {
 pub(super) struct Device {
     dbus_conn: Connection,
     device: DeviceName,
+    dev_model: Option<String>,
     device_conn: Option<DeviceConnection>,
     device_added_stream: DeviceAddedStream<'static>,
     device_removed_stream: DeviceRemovedStream<'static>,
 }
 
 impl Device {
-    pub(super) async fn new(device: DeviceName) -> Result<Self> {
+    pub(super) async fn new(device: DeviceName, dev_model: Option<String>) -> Result<Self> {
         let dbus_conn = new_system_dbus_connection().await?;
 
-        let device_conn = DeviceConnection::new(&dbus_conn, &device).await?;
+        let device_conn = DeviceConnection::new(&dbus_conn, &device, dev_model.as_deref()).await?;
 
         let upower_proxy = UPowerProxy::new(&dbus_conn)
             .await
@@ -107,6 +122,7 @@ impl Device {
         Ok(Self {
             dbus_conn,
             device,
+            dev_model,
             device_conn,
             device_added_stream,
             device_removed_stream,
@@ -178,7 +194,7 @@ impl BatteryDevice for Device {
                     _ = self.device_removed_stream.next() => {},
                     _ = self.device_added_stream.next() => {
                         if let Some(device_conn) =
-                        DeviceConnection::new(&self.dbus_conn, &self.device).await?
+                        DeviceConnection::new(&self.dbus_conn, &self.device, self.dev_model.as_deref()).await?
                         {
                             self.device_conn = Some(device_conn);
                             break;
@@ -205,6 +221,9 @@ trait Device {
 
     #[dbus_proxy(property)]
     fn native_path(&self) -> zbus::Result<String>;
+
+    #[dbus_proxy(property)]
+    fn model(&self) -> zbus::Result<String>;
 
     #[dbus_proxy(property)]
     fn online(&self) -> zbus::Result<bool>;
