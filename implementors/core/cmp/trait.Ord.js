33734bde8d92f439850b156fb56405bd56b61b86
MaxVerevkin <34583604+MaxVerevkin@users.noreply.github.com>
2023-03-10 13:12:04 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@44f75f2b759768bcb658d80dda659dda8f5936b4 🚀

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"i3status_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"i3status_rs/formatting/prefix/enum.Prefix.html\" title=\"enum i3status_rs::formatting::prefix::Prefix\">Prefix</a>"]]
+"i3status_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"i3status_rs/formatting/prefix/enum.Prefix.html\" title=\"enum i3status_rs::formatting::prefix::Prefix\">Prefix</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
