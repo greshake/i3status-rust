44f75f2b759768bcb658d80dda659dda8f5936b4
MaxVerevkin <maxxverrr@gmail.com>
2023-03-10 15:04:49 +0200

    cpu: do not divide by zero

--- src/blocks/cpu.rs
+++ src/blocks/cpu.rs
@@ -210,8 +210,12 @@ impl CpuTime {
     }
 
     fn utilization(&self, old: Self) -> f64 {
-        let elapsed = (self.idle + self.non_idle) as f64 - (old.idle + old.non_idle) as f64;
-        ((self.non_idle - old.non_idle) as f64 / elapsed).clamp(0., 1.)
+        let elapsed = (self.idle + self.non_idle).saturating_sub(old.idle + old.non_idle);
+        if elapsed == 0 {
+            0.0
+        } else {
+            ((self.non_idle - old.non_idle) as f64 / elapsed as f64).clamp(0., 1.)
+        }
     }
 }
 
