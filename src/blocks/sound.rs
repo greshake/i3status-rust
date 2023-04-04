977d98904e12a20b5428eb3317c76db3594c2553
Elbert Ronnie <elbert.ronniep@gmail.com>
2023-04-04 16:40:03 +0530

    remove `mappings_regex` and add `mappings_use_regex`

--- src/blocks/sound.rs
+++ src/blocks/sound.rs
@@ -51,15 +51,15 @@
 //! `max_vol` | Max volume in percent that can be set via scrolling. Note it can still be set above this value if changed by another application. | `None`
 //! `show_volume_when_muted` | Show the volume even if it is currently muted. | `false`
 //! `headphones_indicator` | Change icon when headphones are plugged in (pulseaudio only) | `false`
-//! `mappings` | Map `output_name` to custom name. It has higher priority than `mappings_regex`. | `None`
-//! `mappings_regex` | Map `output_name` that matches a regex to custom name. | `None`
+//! `mappings` | Map `output_name` to custom name. | `None`
+//! `mappings_use_regex` | Let `mappings` match using regex instead of string equality. The replacement will be regex aware and can contain capture groups. | `false`
 //!
 //! Placeholder          | Value                             | Type   | Unit
 //! ---------------------|-----------------------------------|--------|---------------
 //! `icon`               | Icon based on volume              | Icon   | -
 //! `volume`             | Current volume. Missing if muted. | Number | %
 //! `output_name`        | PulseAudio or ALSA device name    | Text   | -
-//! `output_description` | PulseAudio device description, will fallback to `output_name` if no description is available and will be overwritten by mappings or mappings_regex (mappings and mappings_regex will still use `output_name`) | Text | -
+//! `output_description` | PulseAudio device description, will fallback to `output_name` if no description is available and will be overwritten by mappings (mappings will still use `output_name`) | Text | -
 //!
 //! Action        | Default button
 //! --------------|---------------
@@ -81,16 +81,7 @@ mod pulseaudio;
 
 use super::prelude::*;
 use regex::Regex;
-use serde_with::{Map, serde_conv, serde_as};
-
-// A conversion adapter that is used to convert Regex to String and vice versa
-// when serializing or deserializing using serde.
-serde_conv!(
-    RegexAsString,
-    Regex,
-    |regex: &Regex| regex.as_str().to_string(),
-    |value: String| Regex::new(&value)
-);
+use serde_with::{serde_as, Map};
 
 #[serde_as]
 #[derive(Deserialize, Debug, SmartDefault)]
@@ -106,12 +97,15 @@ pub struct Config {
     format: FormatConfig,
     headphones_indicator: bool,
     show_volume_when_muted: bool,
-    mappings: Option<HashMap<String, String>>,
-    // Vec of tuples is used instead of HashMap since ordering of regexes in
-    // file will be important if two regexes could match the same device name.
-    #[serde_as(as = "Option<Map<RegexAsString, _>>")]
-    mappings_regex: Option<Vec<(Regex, String)>>,
+    #[serde_as(as = "Option<Map<_, _>>")]
+    mappings: Option<Vec<(String, String)>>,
+    mappings_use_regex: bool,
     max_vol: Option<u32>,
+}
+
+enum Mappings {
+    Exact(Vec<(String, String)>),
+    Regex(Vec<(Regex, String)>),
 }
 
 pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
@@ -191,25 +185,48 @@ pub async fn run(config: Config, mut api: CommonApi) -> Result<()> {
         )?),
     };
 
+    let mappings = match config.mappings {
+        Some(m) => {
+            if config.mappings_use_regex {
+                Some(Mappings::Regex(
+                    m.into_iter()
+                        .map(|(key, val)| {
+                            Ok((
+                                Regex::new(&key)
+                                    .error("Failed to parse `{key}` in mappings as regex")?,
+                                val,
+                            ))
+                        })
+                        .collect::<Result<_>>()?,
+                ))
+            } else {
+                Some(Mappings::Exact(m))
+            }
+        }
+        None => None,
+    };
+
     loop {
         device.get_info().await?;
         let volume = device.volume();
         let muted = device.muted();
         let mut output_name = device.output_name();
-
-        if let Some(mappings_regex) = &config.mappings_regex {
-            if let Some((regex, mapped)) = mappings_regex
-                .iter()
-                .find(|&(regex, _)| regex.is_match(&output_name))
-            {
-                output_name = regex.replace(&output_name, mapped).into_owned();
+        match &mappings {
+            Some(Mappings::Regex(m)) => {
+                if let Some((regex, mapped)) =
+                    m.iter().find(|(regex, _)| regex.is_match(&output_name))
+                {
+                    output_name = regex.replace(&output_name, mapped).into_owned();
+                }
             }
-        }
-
-        if let Some(mappings) = &config.mappings {
-            if let Some(mapped) = mappings.get(&output_name) {
-                output_name = mapped.clone();
+            Some(Mappings::Exact(m)) => {
+                if let Some((_, mapped)) =
+                    m.iter().find(|&(exact, _)| output_name == exact.as_str())
+                {
+                    output_name = mapped.clone();
+                }
             }
+            None => {}
         }
 
         let output_description = device
