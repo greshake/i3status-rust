cb7b62a503a7cb61e2c20915983e3d47171c0f26
Bryan Malyn <bim9262@gmail.com>
2024-02-23 09:31:41 -0600

    Split formatters into modules

---
+++ src/formatting/formatter/datetime.rs
@@ -0,0 +1,151 @@
+use chrono::format::{Item, StrftimeItems};
+use chrono::{DateTime, Local, Locale, TimeZone};
+use once_cell::sync::Lazy;
+
+use std::fmt::Display;
+
+use super::*;
+
+const DEFAULT_DATETIME_FORMAT: &str = "%a %d/%m %R";
+
+pub static DEFAULT_DATETIME_FORMATTER: Lazy<DatetimeFormatter> =
+    Lazy::new(|| DatetimeFormatter::new(Some(DEFAULT_DATETIME_FORMAT), None).unwrap());
+
+#[derive(Debug)]
+pub enum DatetimeFormatter {
+    Chrono {
+        items: Vec<Item<'static>>,
+        locale: Option<Locale>,
+    },
+    #[cfg(feature = "icu_calendar")]
+    Icu {
+        length: icu_datetime::options::length::Date,
+        locale: icu_locid::Locale,
+    },
+}
+
+fn make_static_item(item: Item<'_>) -> Item<'static> {
+    match item {
+        Item::Literal(str) => Item::OwnedLiteral(str.into()),
+        Item::OwnedLiteral(boxed) => Item::OwnedLiteral(boxed),
+        Item::Space(str) => Item::OwnedSpace(str.into()),
+        Item::OwnedSpace(boxed) => Item::OwnedSpace(boxed),
+        Item::Numeric(numeric, pad) => Item::Numeric(numeric, pad),
+        Item::Fixed(fixed) => Item::Fixed(fixed),
+        Item::Error => Item::Error,
+    }
+}
+
+impl DatetimeFormatter {
+    pub(super) fn from_args(args: &[Arg]) -> Result<Self> {
+        let mut format = None;
+        let mut locale = None;
+        for arg in args {
+            match arg.key {
+                "format" | "f" => {
+                    format = Some(arg.val);
+                }
+                "locale" | "l" => {
+                    locale = Some(arg.val);
+                }
+                other => {
+                    return Err(Error::new(format!(
+                        "Unknown argument for 'datetime': '{other}'"
+                    )));
+                }
+            }
+        }
+        Self::new(format, locale)
+    }
+
+    fn new(format: Option<&str>, locale: Option<&str>) -> Result<Self> {
+        let (items, locale) = match locale {
+            Some(locale) => {
+                #[cfg(feature = "icu_calendar")]
+                let Ok(locale) = locale.try_into() else {
+                    use std::str::FromStr as _;
+                    // try with icu4x
+                    let locale = icu_locid::Locale::from_str(locale)
+                        .ok()
+                        .error("invalid locale")?;
+                    let length = match format {
+                        Some("full") => icu_datetime::options::length::Date::Full,
+                        None | Some("long") => icu_datetime::options::length::Date::Long,
+                        Some("medium") => icu_datetime::options::length::Date::Medium,
+                        Some("short") => icu_datetime::options::length::Date::Short,
+                        _ => return Err(Error::new("Unknown format option for icu based locale")),
+                    };
+                    return Ok(Self::Icu { locale, length });
+                };
+                #[cfg(not(feature = "icu_calendar"))]
+                let locale = locale.try_into().ok().error("invalid locale")?;
+                (
+                    StrftimeItems::new_with_locale(
+                        format.unwrap_or(DEFAULT_DATETIME_FORMAT),
+                        locale,
+                    ),
+                    Some(locale),
+                )
+            }
+            None => (
+                StrftimeItems::new(format.unwrap_or(DEFAULT_DATETIME_FORMAT)),
+                None,
+            ),
+        };
+
+        Ok(Self::Chrono {
+            items: items.map(make_static_item).collect(),
+            locale,
+        })
+    }
+}
+
+impl Formatter for DatetimeFormatter {
+    fn format(&self, val: &Value, _config: &SharedConfig) -> Result<String, FormatError> {
+        fn for_generic_datetime<T>(
+            this: &DatetimeFormatter,
+            datetime: DateTime<T>,
+        ) -> Result<String, FormatError>
+        where
+            T: TimeZone,
+            T::Offset: Display,
+        {
+            Ok(match this {
+                DatetimeFormatter::Chrono { items, locale } => match *locale {
+                    Some(locale) => datetime.format_localized_with_items(items.iter(), locale),
+                    None => datetime.format_with_items(items.iter()),
+                }
+                .to_string(),
+                #[cfg(feature = "icu_calendar")]
+                DatetimeFormatter::Icu { locale, length } => {
+                    use chrono::Datelike as _;
+                    let date = icu_calendar::Date::try_new_iso_date(
+                        datetime.year_ce().1 as i32,
+                        datetime.month() as u8,
+                        datetime.day() as u8,
+                    )
+                    .ok()
+                    .error("Current date should be a valid date")?;
+                    let date = date.to_any();
+                    let dft =
+                        icu_datetime::DateFormatter::try_new_with_length(&locale.into(), *length)
+                            .ok()
+                            .error("locale should be present in compiled data")?;
+                    dft.format_to_string(&date)
+                        .ok()
+                        .error("formatting date using icu failed")?
+                }
+            })
+        }
+        match val {
+            Value::Datetime(datetime, timezone) => match timezone {
+                Some(tz) => for_generic_datetime(self, datetime.with_timezone(tz)),
+                None => for_generic_datetime(self, datetime.with_timezone(&Local)),
+            },
+            other => Err(FormatError::IncompatibleFormatter {
+                ty: other.type_name(),
+                fmt: "datetime",
+            }),
+        }
+    }
+}
---
+++ src/formatting/formatter/eng.rs
@@ -0,0 +1,313 @@
+use crate::formatting::prefix::Prefix;
+use crate::formatting::unit::Unit;
+
+use super::*;
+
+const DEFAULT_NUMBER_WIDTH: usize = 2;
+const DEFAULT_NUMBER_PAD_WITH: char = ' ';
+
+// TODO: split those defaults
+pub const DEFAULT_NUMBER_FORMATTER: EngFormatter = EngFormatter {
+    width: DEFAULT_NUMBER_WIDTH,
+    unit: None,
+    unit_has_space: false,
+    unit_hidden: false,
+    prefix: None,
+    prefix_has_space: false,
+    prefix_hidden: false,
+    prefix_forced: false,
+    pad_with: DEFAULT_NUMBER_PAD_WITH,
+};
+
+#[derive(Debug)]
+pub struct EngFormatter {
+    width: usize,
+    unit: Option<Unit>,
+    unit_has_space: bool,
+    unit_hidden: bool,
+    prefix: Option<Prefix>,
+    prefix_has_space: bool,
+    prefix_hidden: bool,
+    prefix_forced: bool,
+    pad_with: char,
+}
+
+impl EngFormatter {
+    pub(super) fn from_args(args: &[Arg]) -> Result<Self> {
+        let mut width = DEFAULT_NUMBER_WIDTH;
+        let mut unit = None;
+        let mut unit_has_space = false;
+        let mut unit_hidden = false;
+        let mut prefix = None;
+        let mut prefix_has_space = false;
+        let mut prefix_hidden = false;
+        let mut prefix_forced = false;
+        let mut pad_with = DEFAULT_NUMBER_PAD_WITH;
+
+        for arg in args {
+            match arg.key {
+                "width" | "w" => {
+                    width = arg.val.parse().error("Width must be a positive integer")?;
+                }
+                "unit" | "u" => {
+                    unit = Some(arg.val.parse()?);
+                }
+                "hide_unit" => {
+                    unit_hidden = arg
+                        .val
+                        .parse()
+                        .ok()
+                        .error("hide_unit must be true or false")?;
+                }
+                "unit_space" => {
+                    unit_has_space = arg
+                        .val
+                        .parse()
+                        .ok()
+                        .error("unit_space must be true or false")?;
+                }
+                "prefix" | "p" => {
+                    prefix = Some(arg.val.parse()?);
+                }
+                "hide_prefix" => {
+                    prefix_hidden = arg
+                        .val
+                        .parse()
+                        .ok()
+                        .error("hide_prefix must be true or false")?;
+                }
+                "prefix_space" => {
+                    prefix_has_space = arg
+                        .val
+                        .parse()
+                        .ok()
+                        .error("prefix_space must be true or false")?;
+                }
+                "force_prefix" => {
+                    prefix_forced = arg
+                        .val
+                        .parse()
+                        .ok()
+                        .error("force_prefix must be true or false")?;
+                }
+                "pad_with" => {
+                    pad_with = arg
+                        .val
+                        .parse()
+                        .error("pad_with must be a single character")?;
+                }
+                other => {
+                    return Err(Error::new(format!("Unknown argument for 'eng': '{other}'")));
+                }
+            }
+        }
+
+        Ok(Self {
+            width,
+            unit,
+            unit_has_space,
+            unit_hidden,
+            prefix,
+            prefix_has_space,
+            prefix_hidden,
+            prefix_forced,
+            pad_with,
+        })
+    }
+}
+
+impl Formatter for EngFormatter {
+    fn format(&self, val: &Value, _config: &SharedConfig) -> Result<String, FormatError> {
+        match val {
+            Value::Number { mut val, mut unit } => {
+                let is_negative = val.is_sign_negative();
+                if is_negative {
+                    val = -val;
+                }
+
+                if let Some(new_unit) = self.unit {
+                    val = unit.convert(val, new_unit)?;
+                    unit = new_unit;
+                }
+
+                let (min_prefix, max_prefix) = match (self.prefix, self.prefix_forced) {
+                    (Some(prefix), true) => (prefix, prefix),
+                    (Some(prefix), false) => (prefix, Prefix::max_available()),
+                    (None, _) => (Prefix::min_available(), Prefix::max_available()),
+                };
+
+                let prefix = unit
+                    .clamp_prefix(if min_prefix.is_binary() {
+                        Prefix::eng_binary(val)
+                    } else {
+                        Prefix::eng(val)
+                    })
+                    .clamp(min_prefix, max_prefix);
+                val = prefix.apply(val);
+
+                let mut digits = (val.max(1.).log10().floor() + 1.0) as i32 + is_negative as i32;
+
+                // handle rounding
+                if self.width as i32 - digits >= 1 {
+                    let round_up_to = self.width as i32 - digits - 1;
+                    let m = 10f64.powi(round_up_to);
+                    val = (val * m).round() / m;
+                    digits = (val.max(1.).log10().floor() + 1.0) as i32 + is_negative as i32;
+                }
+
+                let sign = if is_negative { "-" } else { "" };
+                let mut retval = match self.width as i32 - digits {
+                    i32::MIN..=0 => format!("{sign}{}", val.round()),
+                    1 => format!("{}{sign}{}", self.pad_with, val.round() as i64),
+                    rest => format!("{sign}{val:.*}", rest as usize - 1),
+                };
+
+                let display_prefix =
+                    !self.prefix_hidden && prefix != Prefix::One && prefix != Prefix::OneButBinary;
+                let display_unit = !self.unit_hidden && unit != Unit::None;
+
+                if display_prefix {
+                    if self.prefix_has_space {
+                        retval.push(' ');
+                    }
+                    retval.push_str(&prefix.to_string());
+                }
+                if display_unit {
+                    if self.unit_has_space || (self.prefix_has_space && !display_prefix) {
+                        retval.push(' ');
+                    }
+                    retval.push_str(&unit.to_string());
+                }
+
+                Ok(retval)
+            }
+            other => Err(FormatError::IncompatibleFormatter {
+                ty: other.type_name(),
+                fmt: "eng",
+            }),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! fmt {
+        ($name:ident, $($key:ident : $value:tt),*) => {
+            new_formatter(stringify!($name), &[
+                $( Arg { key: stringify!($key), val: stringify!($value) } ),*
+            ]).unwrap()
+        };
+    }
+
+    #[test]
+    fn eng_rounding_and_negatives() {
+        let fmt = fmt!(eng, w: 3);
+        let config = SharedConfig::default();
+
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: -1.0,
+                    unit: Unit::None,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, " -1");
+
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: 9.9999,
+                    unit: Unit::None,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, " 10");
+
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: 999.9,
+                    unit: Unit::Bytes,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, "1.0KB");
+
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: -9.99,
+                    unit: Unit::None,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, "-10");
+
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: 9.94,
+                    unit: Unit::None,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, "9.9");
+
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: 9.95,
+                    unit: Unit::None,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, " 10");
+
+        let fmt = fmt!(eng, w: 5, p: 1);
+        let result = fmt
+            .format(
+                &Value::Number {
+                    val: 321_600_000_000.,
+                    unit: Unit::Bytes,
+                },
+                &config,
+            )
+            .unwrap();
+        assert_eq!(result, "321.6GB");
+    }
+
+    #[test]
+    fn eng_prefixes() {
+        let config = SharedConfig::default();
+        // 14.96 GiB
+        let val = Value::Number {
+            val: 14.96 * 1024. * 1024. * 1024.,
+            unit: Unit::Bytes,
+        };
+
+        let fmt = fmt!(eng, w: 5, p: Mi);
+        let result = fmt.format(&val, &config).unwrap();
+        assert_eq!(result, "14.96GiB");
+
+        let fmt = fmt!(eng, w: 4, p: Mi);
+        let result = fmt.format(&val, &config).unwrap();
+        assert_eq!(result, "15.0GiB");
+
+        let fmt = fmt!(eng, w: 3, p: Mi);
+        let result = fmt.format(&val, &config).unwrap();
+        assert_eq!(result, " 15GiB");
+
+        let fmt = fmt!(eng, w: 2, p: Mi);
+        let result = fmt.format(&val, &config).unwrap();
+        assert_eq!(result, "15GiB");
+    }
+}
