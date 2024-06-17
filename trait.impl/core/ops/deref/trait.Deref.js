0c30c49979a209a40c53d0df361720460ac00ba7
MaxVerevkin <34583604+MaxVerevkin@users.noreply.github.com>
2024-06-17 07:07:17 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@47d068710403bca5ac662156cc639289dbffc4bf 🚀

--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"i3status_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"i3status_rs/themes/struct.Theme.html\" title=\"struct i3status_rs::themes::Theme\">Theme</a>"]]
+"i3status_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"i3status_rs/themes/struct.Theme.html\" title=\"struct i3status_rs::themes::Theme\">Theme</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/core/ops/deref/trait.DerefMut.js
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"i3status_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"i3status_rs/themes/struct.Theme.html\" title=\"struct i3status_rs::themes::Theme\">Theme</a>"]]
+"i3status_rs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"i3status_rs/themes/struct.Theme.html\" title=\"struct i3status_rs::themes::Theme\">Theme</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
