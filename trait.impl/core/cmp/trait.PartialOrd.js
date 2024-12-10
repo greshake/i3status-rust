68a15272bacbb6afa94921e90f42902eba1a325f
MaxVerevkin <34583604+MaxVerevkin@users.noreply.github.com>
2024-12-10 08:11:29 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@9f52b653f7ce8cfe4d10aa07d14e25b33897abbb 🚀

--- trait.impl/core/cmp/trait.PartialOrd.js
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"i3status_rs/formatting/prefix/enum.Prefix.html\" title=\"enum i3status_rs::formatting::prefix::Prefix\">Prefix</a>"]]]]);
+    var implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"i3status_rs/formatting/prefix/enum.Prefix.html\" title=\"enum i3status_rs::formatting::prefix::Prefix\">Prefix</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
