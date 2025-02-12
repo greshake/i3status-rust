bb26f663a59a3cf1505236719274675dd42c0f6d
bim9262 <bim9262@gmail.com>
2025-02-12 02:05:31 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@0afe30deb07a76e2ec2541399d3f5ad0fe523f81 🚀

--- i3status_rs/blocks/packages/sidebar-items.js
+++ i3status_rs/blocks/packages/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["PackageManager"],"fn":["has_matching_update","run"],"mod":["apt","dnf","pacman"],"struct":["Config"],"trait":["Backend"]};
+window.SIDEBAR_ITEMS = {"enum":["PackageManager"],"fn":["has_matching_update","run"],"mod":["apt","dnf","pacman","xbps"],"struct":["Config"],"trait":["Backend"]};
