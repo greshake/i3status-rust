4df7cdba69e1d3d4e5f4d0fc809fac660209ceff
bim9262 <bim9262@gmail.com>
2025-04-26 03:00:20 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@115fed06d6df768afa3d1d78e3ed897b89f5599e 🚀

--- i3status_rs/sidebar-items.js
+++ i3status_rs/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["map"],"mod":["blocks","click","config","errors","escape","formatting","icons","protocol","themes","util","widget"],"struct":["BarState","Block","CliArgs"]};
+window.SIDEBAR_ITEMS = {"macro":["map"],"mod":["blocks","click","config","errors","escape","formatting","geolocator","icons","protocol","themes","util","widget"],"struct":["BarState","Block","CliArgs"]};
