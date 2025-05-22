67bd6a9e1d951db8a24575d8141d88bd147a4527
bim9262 <bim9262@gmail.com>
2025-05-22 21:16:48 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@544d855e756d16b621fcc762dc85f075b29e43f9 🚀

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"i3status_rs/blocks/sound/enum.DeviceKind.html\" title=\"enum i3status_rs::blocks::sound::DeviceKind\">DeviceKind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"i3status_rs/click/enum.MouseButton.html\" title=\"enum i3status_rs::click::MouseButton\">MouseButton</a>"]]]]);
+    var implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"i3status_rs/blocks/sound/enum.DeviceKind.html\" title=\"enum i3status_rs::blocks::sound::DeviceKind\">DeviceKind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"i3status_rs/click/enum.MouseButton.html\" title=\"enum i3status_rs::click::MouseButton\">MouseButton</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
