ff0d3bc24828ae9df94628e17f6b49fd8e385108
bim9262 <bim9262@gmail.com>
2025-06-27 01:49:29 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@1749515d23cb3aead7e96749626798c806f711bf 🚀

--- trait.impl/core/ops/deref/trait.DerefMut.js
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"i3status_rs/themes/struct.Theme.html\" title=\"struct i3status_rs::themes::Theme\">Theme</a>"]]]]);
+    var implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"i3status_rs/themes/struct.Theme.html\" title=\"struct i3status_rs::themes::Theme\">Theme</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
