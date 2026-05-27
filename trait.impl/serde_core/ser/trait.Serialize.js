2b33233bc6234f0dc874aafdfcc79196df0783e8
bim9262 <bim9262@gmail.com>
2026-05-27 23:38:43 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@188cfd660a23d6b35b4ececacbd036eb65cf39a0 🚀

---
+++ trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"i3status_rs/protocol/i3bar_block/enum.I3BarBlockAlign.html\" title=\"enum i3status_rs::protocol::i3bar_block::I3BarBlockAlign\">I3BarBlockAlign</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"i3status_rs/protocol/i3bar_block/enum.I3BarBlockMinWidth.html\" title=\"enum i3status_rs::protocol::i3bar_block::I3BarBlockMinWidth\">I3BarBlockMinWidth</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"i3status_rs/themes/color/enum.Color.html\" title=\"enum i3status_rs::themes::color::Color\">Color</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"i3status_rs/protocol/i3bar_block/struct.I3BarBlock.html\" title=\"struct i3status_rs::protocol::i3bar_block::I3BarBlock\">I3BarBlock</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1347]}
