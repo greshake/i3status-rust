1194283fa73a8b140b3d6b9280fc777dccc23797
bim9262 <bim9262@gmail.com>
2026-06-13 17:44:05 +0000

    Deploying to gh-pages from @ greshake/i3status-rust@7e440cb6e24a47982c3e092db0b0b4271d55e268 🚀

---
+++ trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["i3status_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"i3status_rs/blocks/weather/enum.UnitSystem.html\" title=\"enum i3status_rs::blocks::weather::UnitSystem\">UnitSystem</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[419]}
